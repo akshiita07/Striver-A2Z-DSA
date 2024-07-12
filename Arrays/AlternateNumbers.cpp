ddf2c8575bd23e620b847ac75dbdd6b2226de177
Akshita Pathak <pathakshita07@gmail.com>
2024-07-12 12:21:35 +0530

    Arrays

---
+++ Arrays/AlternateNumbers.cpp
@@ -0,0 +1,60 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+// no of +&- are not same-- so add remaining at end without altering order
+void alternateNumbers(int arr[], int n)
+{
+    vector<int> ans;
+    vector<int> positive;
+    vector<int> negative;
+    // TIME-O(n)- GET +&-VES
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] >= 0)
+        {
+            positive.push_back(arr[i]);
+        }
+        else
+        {
+            negative.push_back(arr[i]);
+        }
+    }
+    int i = 0;
+    int j = 0;
+    // TIME-O(n)
+    while (i < positive.size() && j < negative.size())
+    {
+        ans.push_back(positive[i]);
+        i++;
+        ans.push_back(negative[j]);
+        j++;
+    }
+    // for remaning
+    // TIME-O(n)
+    for (; i < positive.size(); i++)
+    {
+        ans.push_back(positive[i]);
+    }
+    for (; j < negative.size(); j++)
+    {
+        ans.push_back(negative[j]);
+    }
+
+    // print modified array
+    cout << "\nThe modified array is-\n";
+    for (auto it : ans)
+    {
+        cout << it << " ";
+    }
+}
+// TOTAL TIME-O(  )
+
+int main()
+{
+    int arr[] = {1, 2, -4, -5, 3, 4};
+    // int arr[] = {1, 2, -3, -1, -2, -3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    alternateNumbers(arr, n);
+
+    return 0;
+}
---
+++ Leetcode/2149RearrangeArrayElementsbySign.cpp
@@ -0,0 +1,83 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+// BRUTE- TIME-O(n)+O(n)            SPACE-O(n)
+void rearrangeArray_brute(int arr[], int n)
+{
+    vector<int> ans;
+    vector<int> positive;
+    vector<int> negative;
+
+    // add all + nos from arr to positive array
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] >= 0)
+        {
+            positive.push_back(arr[i]);
+        }
+        else
+        {
+            negative.push_back(arr[i]);
+        }
+    }
+
+    // add these +,- in order in ans array
+    int i = 0;
+    int j = 0;
+    while (i < positive.size() && j < negative.size())
+    {
+        ans.push_back(positive[i]);
+        i++;
+        ans.push_back(negative[j]);
+        j++;
+    }
+    // print modified array
+    cout << "\nThe modified array is-\n";
+    for (auto it : ans)
+    {
+        cout << it << " ";
+    }
+}
+
+// OPTIMAL - 1 PASS- TIME-O(n)            SPACE-O(n)
+void rearrangeArray_optimal(int arr[], int n)
+{
+    vector<int> ans(n);
+    int posInd=0;
+    int negInd=1;
+
+    // add all + nos from arr to positive array
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i]>=0)
+        {
+            // positive no
+            ans[posInd]=arr[i];
+            // add positive no at even place
+            posInd+=2;
+        }
+        else
+        {
+            // add negative no at ODD place
+            ans[negInd]=arr[i];
+            negInd+=2;
+        }
+    }
+    
+    // print modified array
+    cout << "\nThe modified array is-\n";
+    for (auto it : ans)
+    {
+        cout << it << " ";
+    }
+}
+
+int main()
+{
+    int arr[] = {3, 1, -2, -5, 2, -4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    // rearrangeArray_brute(arr, n);
+    rearrangeArray_optimal(arr, n);
+
+    return 0;
+}
