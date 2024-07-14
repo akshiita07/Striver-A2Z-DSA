3cb391c77d3afd1da3641f0728a12c6c5f47aeed
Akshita Pathak <pathakshita07@gmail.com>
2024-07-14 22:30:13 +0530

    Arrays

--- Leetcode/31NextPermutation.cpp
+++ Leetcode/31NextPermutation.cpp
@@ -4,16 +4,75 @@ using namespace std;
 // BRUTE FORCE- GENERATE ALL PERMUTATIONS, LINEAR SEARCH FOR GIVEN arr PERMUTATION, THEN RETURN NEXT PERMUTATION
 // time-  O(n!)       space- O()
 // for 3 numbers-> 3! no of permutations possible
+void permutationsRecursive(int arr[], int n, vector<int> &ds, vector<vector<int>> &ans, vector<bool> &hashMap)
+{
+
+    // BASE CASE:
+    //  when size of ds==n then ans
+    if (ds.size() == n)
+    {
+        ans.push_back(ds);
+        return;
+    }
+
+    // a map is reqd to mark which element is already picked
+    for (int i = 0; i < n; i++)
+    {
+        // if i in map is NOT MARKED then add in data str & mark it in mao
+        if (!hashMap[i])
+        { // not marked
+            // put in ds
+            ds.push_back(arr[i]);
+            // mark in map
+            hashMap[i] = true;
+            // recursive
+            permutationsRecursive(arr, n, ds, ans, hashMap);
+
+            // remove whatever picked & false
+            ds.pop_back();
+            hashMap[i] = false;
+        }
+        // if map is MARKED
+    }
+} // TIME-O(n*n!)         space-O(n+n)-->extra map array+ds
 void nextPermutation_Brute(int arr[], int n)
 {
-    vector<int> ans;
+    vector<vector<int>> ans; // will store arrays of permutations
+    vector<int> ds;
+    vector<bool> hashMap(n, 0); // of size n ie initialised to 0
 
     // WRITE PERMUTATIONS IN SORTED ORDER-
+    permutationsRecursive(arr, n, ds, ans, hashMap);
 
-    cout << "\nThe next lexicographic permutation is:\n";
+    cout << "\nPermutations are:\n";
     for (auto it : ans)
     {
-        cout << it << " ";
+        for (auto ele : it)
+        {
+            cout << ele << " ";
+        }
+        cout << "\n";
+    }
+
+    cout << "\nThe next lexicographic permutation is:\n";
+    // linear search
+    bool found = false;
+    for (auto i = 0; i < ans.size(); i++)
+    {
+        if (found)
+        {
+            for (auto ele : ans[i])
+            {
+                cout << ele << " ";
+            }
+            cout << "\n";
+            return;
+        }
+
+        if (equal(arr, arr + n, ans[i].begin()))        //if elements in arr r equal
+        {
+            found = true;
+        }
     }
 }
 
@@ -46,11 +105,11 @@ void nextPermutation_Optimal(int arr[], int n)
 int main()
 {
     int arr[] = {1, 2, 3};
-    int n=sizeof(arr)/sizeof(arr[0]);
-    // nextPermutation_Brute(arr, n);
+    int n = sizeof(arr) / sizeof(arr[0]);
+    nextPermutation_Brute(arr, n);
 
     vector<int> vec = {1, 2, 3};
-    nextPermutation_Stl(vec);
+    // nextPermutation_Stl(vec);
 
     return 0;
 }
