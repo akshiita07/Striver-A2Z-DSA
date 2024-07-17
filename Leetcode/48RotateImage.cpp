a824ea8a75138db2b7e3933151c824621d9c4537
Akshita Pathak <pathakshita07@gmail.com>
2024-07-17 11:31:16 +0530

    Arrays

--- Leetcode/48RotateImage.cpp
+++ Leetcode/48RotateImage.cpp
@@ -27,12 +27,49 @@ void rotate_brute(vector<vector<int>> &matrix, int n)
     }
 }
 
+// OPTIMAL: TIME:O(n*n)    SPAVE:O()-->IN PLACE-> WITHIN D GIVEN MATRIX
+void rotate_optimal(vector<vector<int>> &matrix, int n)
+{
+    // STEP 1: TAKE TRANSPOSE OF MATRIX-> MAKE COLM AS ROW
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            swap(matrix[i][j],matrix[j][i]);
+        }
+    }
+    // STEP 2: REVERSE EACH ROW OF MATRIX
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n/2; j++)
+    //     {
+    //         swap(matrix[i][j],matrix[i][n-j-1]);
+    //     }
+    // }
+    for (int i = 0; i < n; i++)
+    {
+        reverse(matrix[i].begin(),matrix[i].end());
+    }
+
+    // print matrix
+    cout << "\nAfter 90° rotation:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
-    // vector<vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    vector<vector<int>> matrix = {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+    vector<vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    // vector<vector<int>> matrix = {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
     int n = matrix.size();
-    rotate_brute(matrix, n);
+    // rotate_brute(matrix, n);
+    rotate_optimal(matrix, n);
 
     return 0;
 }
---
+++ Leetcode/54SpiralMatrix.cpp
@@ -0,0 +1,75 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+// SPIRAL ORDER TRAVERSAL:
+//                                ➡️➡️➡️➡️➡️➡️
+//                                            ⬇️
+//                                ⬆️➡️➡️➡️   ⬇️
+//                                ⬆️          ⬇️
+//                                ⬅️⬅️⬅️⬅️⬇️⬅️
+
+// ONLY 1 POSSIBLE SOLN- OPTIMAL
+void spiralOrder_optimal(vector<vector<int>> &matrix, int m, int n)
+{
+    vector<int> ans;
+
+    // right,bottom,left,top
+    int top = 0;        // top row
+    int bottom = m - 1; // bottom row so m-1
+    int right = n - 1;  // rightmost col so n-1
+    int left = 0;       // left column
+
+    // jab tak elements / rows hai tab tak :
+    while (left <= right && top <= bottom)
+    {
+        // NO CHANGE IN PARAMETERS UPTO HERE SO NO CONDITION CHECKS REQD:
+        for (int i = left; i <= right; i++)
+        {
+            // print from left to right--> row is printed
+            ans.push_back(matrix[top][i]);
+        }
+        top++;
+        for (int i = top; i <= bottom; i++)
+        {
+            // print from top+1 to bottom--> colm is printed
+            ans.push_back(matrix[i][right]);
+        }
+        right--;
+
+        // NOW BOTH TOP & RIGHT HAVE CHANGED SO CONDITION HAS TO BE CHECKED FIRST!
+
+        if (top <= bottom)
+        {
+            for (int i = right; i >= left; i--)
+            {
+                // print from right-1 to left--> row is printed
+                ans.push_back(matrix[bottom][i]);
+            }
+            bottom--;
+        }
+        if (left <= right)
+        {
+            for (int i = bottom; i >= top; i--)
+            {
+                // print from bottom-1 to top--> colm is printed
+                ans.push_back(matrix[i][left]);
+            }
+            left++;
+        }
+    }
+
+    cout << "\nSpiral order traversal of matrix is:" << endl;
+    for (auto it : ans)
+    {
+        cout << it << " ";
+    }
+}
+
+int main()
+{
+    vector<vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int m = matrix.size();    // rows
+    int n = matrix[0].size(); // colms
+    spiralOrder_optimal(matrix, m, n);
+
+    return 0;
+}
