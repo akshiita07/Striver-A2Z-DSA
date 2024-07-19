877d5f81f14cd11975af72079a59981898e3cfc1
Akshita Pathak <pathakshita07@gmail.com>
2024-07-19 18:11:24 +0530

    Arrays

---
+++ Arrays/SubarrayswithXORK.cpp
@@ -0,0 +1,82 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+// find the number of subarrays of array ‘arr’ whose bitwise XOR( ⊕ ) of all elements is equal to ‘B’.
+// XOR 0^0=0        O^A=A
+
+// BRUTE FORCE:     TIME=O(n^3)     SPACE=O(1)
+int subarraysWithSumK_brute(vector<int> arr, int b)
+{
+    int n = arr.size();
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            int xorAns = 0;
+            for (int k = i; k <= j; k++)
+            {
+                xorAns = xorAns ^ arr[k];
+            }
+            if (xorAns == b)
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+// BETTER:     TIME=O(n^2)     SPACE=O(1)
+int subarraysWithSumK_better(vector<int> arr, int b)
+{
+    int n = arr.size();
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int xorAns = 0;
+        for (int j = i; j < n; j++)
+        {
+            xorAns = xorAns ^ arr[j];
+            if (xorAns == b)
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+// OPTIMAL:     TIME=O()     SPACE=O(1)
+// find x such that  x^k=XR         -> x=XR^k
+// so to keep track of x -> use hash map
+int subarraysWithSumK_optimal(vector<int> arr, int k)
+{
+    int n = arr.size();
+    int count = 0;
+    unordered_map<int,int> hashMap;     //xor,count
+    int xorAns=0;
+    hashMap[0]=1;
+
+    for(int i=0;i<n;i++){
+        xorAns=xorAns^arr[i];
+
+        // find if x exists such that x=k^xorAns
+        int x=k^xorAns;
+        count+=hashMap[x];
+
+        // add in map
+        hashMap[xorAns]++;
+    }
+    return count;
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 2};
+    int b = 2;
+    // cout << "No of subarrays in arr with XOR as " << b << " are: " << subarraysWithSumK_brute(arr, b);
+    // cout << "No of subarrays in arr with XOR as " << b << " are: " << subarraysWithSumK_better(arr, b);
+    cout << "No of subarrays in arr with XOR as " << b << " are: " << subarraysWithSumK_optimal(arr, b);
+
+    return 0;
+}
---
+++ Leetcode/184Sum.cpp
@@ -0,0 +1,170 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+// nums[a] + nums[b] + nums[c] + nums[d] == target & a,b,c,d are unique
+
+// BRUTE FORCE: TIME-O(n^4)     SPACE:O(n)->temp array
+void fourSum_brute(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    vector<vector<int>> ans;
+
+    for (int i = 0; i < n - 3; i++)
+    {
+        for (int j = i + 1; j < n - 2; j++)
+        {
+            for (int k = j + 1; k < n - 1; k++)
+            {
+                for (int l = k + 1; l < n; l++)
+                {
+                    if (nums[i] + nums[j] + nums[k] + nums[l] == target)
+                    {
+                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                        sort(temp.begin(), temp.end());
+                        // check for duplicate
+                        if (find(ans.begin(), ans.end(), temp) == ans.end())
+                        {
+                            // ie not found then add
+                            ans.push_back(temp);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    cout << "The 4 different numbers such that when added=" << target << " are: \n";
+    for (auto row : ans)
+    {
+        for (auto it : row)
+        {
+            cout << it << " ";
+        }
+        cout << endl;
+    }
+}
+
+// BETTER:hash set TIME-O(n^3*1)-for unordered map     SPACE:O(n)->for set
+void fourSum_better(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    vector<vector<int>> ans;
+
+    for (int i = 0; i < n - 2; i++)
+    {
+        for (int j = i + 1; j < n - 1; j++)
+        {
+            unordered_set<int> hash;
+            for (int k = j + 1; k < n; k++)
+            {
+                int forth = target - (nums[i] + nums[j] + nums[k]);
+                // check if forth exists in hash
+                if (hash.find(forth) != hash.end())
+                {
+                    // found
+                    vector<int> temp = {nums[i], nums[j], nums[k], forth};
+                    sort(temp.begin(), temp.end());
+                    // check for duplicate
+                    if (find(ans.begin(), ans.end(), temp) == ans.end())
+                    {
+                        // ie not found then add
+                        ans.push_back(temp);
+                    }
+                }
+                else
+                {
+                    // not present then add it
+                    hash.insert(nums[k]);
+                }
+            }
+        }
+    }
+
+    cout << "The 4 different numbers such that when added=" << target << " are: \n";
+    for (auto row : ans)
+    {
+        for (auto it : row)
+        {
+            cout << it << " ";
+        }
+        cout << endl;
+    }
+}
+
+// OPTIMAL:  TIME-O(n^2)+O(n)-for k,l while loop==O(n^3)time     SPACE:O(1)->no extra space
+void fourSum_optimal(vector<int> &nums, int target)
+{
+    // initially sort array
+    sort(nums.begin(), nums.end());
+    // -2   -1   0   0   1   2
+    int n = nums.size();
+    vector<vector<int>> ans;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        // until same j ->loop
+        if (i > 0 && nums[i] == nums[i - 1])
+        {
+            continue;
+        }
+        for (int j = i + 1; j < n; j++)
+        {
+            // until same j ->loop
+            if (j != i + 1 && nums[j] == nums[j - 1])
+            {
+                continue;
+            }
+            int k = j + 1;
+            int l = n - 1;
+            while (k < l)
+            {
+                int sum = nums[i] + nums[j] + nums[k] + nums[l];
+                if (sum > target)
+                {
+                    l--;
+                }
+                else if (sum < target)
+                {
+                    k++;
+                }
+                else if (sum == target)
+                {
+                    ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+                    // k++ & l--
+                    int prevK = nums[k];
+                    int prevL = nums[l];
+                    if (nums[k] == prevK && k < l)
+                    {
+                        k++;
+                    }
+                    if (nums[l] == prevL && k < l)
+                    {
+                        l--;
+                    }
+                }
+            }
+        }
+    }
+
+    cout << "The 4 different numbers such that when added=" << target << " are: \n";
+    for (auto row : ans)
+    {
+        for (auto it : row)
+        {
+            cout << it << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    vector<int> arr = {1, 0, -1, 0, -2, 2};
+    int target = 0;
+    // Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+
+    // fourSum_brute(arr, target);
+    // fourSum_better(arr, target);
+    fourSum_optimal(arr, target);
+
+    return 0;
+}
