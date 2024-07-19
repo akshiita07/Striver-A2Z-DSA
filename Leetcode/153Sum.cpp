6e8ba2875491bb7491d43205b378a7c04d4b2b3a
Akshita Pathak <pathakshita07@gmail.com>
2024-07-19 11:25:29 +0530

    Arrays

---
+++ Leetcode/153Sum.cpp
@@ -0,0 +1,165 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+// find 3 different numbers such that when added=0
+
+// BRUTE FORCE: TIME-O(n^3)     SPACE:O(n)->temp
+void threeSum_brute(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<vector<int>> ans;
+
+    for (int i = 0; i < n - 2; i++)
+    {
+        for (int j = i + 1; j < n - 1; j++)
+        {
+            for (int k = j + 1; k < n; k++)
+            {
+                if (nums[i] + nums[j] + nums[k] == 0)
+                {
+                    vector<int> temp = {nums[i], nums[j], nums[k]};
+                    sort(temp.begin(), temp.end());
+                    // check for duplicate
+                    if (find(ans.begin(), ans.end(), temp) == ans.end())
+                    {
+                        // ie not found then add
+                        ans.push_back(temp);
+                    }
+                }
+            }
+        }
+    }
+
+    cout << "The 3 different numbers such that when added=0 are: \n";
+    for (auto row : ans)
+    {
+        for (auto it : row)
+        {
+            cout << it << " ";
+        }
+        cout << endl;
+    }
+}
+
+// BETTER:HASH SET TIME-O(n^2)      SPACE:2O(n)->set+temp array
+void threeSum_better(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<vector<int>> ans;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        set<int> hashSet; // only unique
+        for (int j = i + 1; j < n; j++)
+        {
+
+            int k = -(nums[i] + nums[j]);
+            // check if arr[k] exists in set
+            if (hashSet.find(k) != hashSet.end())
+            {
+                // found in map
+                vector<int> temp = {nums[i], nums[j], k};
+                sort(temp.begin(), temp.end());
+                if (find(ans.begin(), ans.end(), temp) == ans.end())
+                {
+                    // ie not found then add
+                    ans.push_back(temp);
+                }
+            }
+            else
+            {
+                // does not exist
+                hashSet.insert(nums[j]);
+            }
+        }
+    }
+
+    cout << "The 3 different numbers such that when added=0 are: \n";
+    for (auto row : ans)
+    {
+        for (auto it : row)
+        {
+            cout << it << " ";
+        }
+        cout << endl;
+    }
+}
+
+// OPTIMAL:2POINTER      TIME-O(nlogn)->sort array +O(n)->i loop +O(n)->while loop so time:O(n^2)      SPACE:O(1)->no extra space reqd
+void threeSum_optimal(vector<int> &nums)
+{
+    sort(nums.begin(), nums.end()); // sort array
+
+    int n = nums.size();
+    vector<vector<int>> ans;
+
+    // if j crosses k then i++
+    for (int i = 0; i < n; i++)
+    {
+        // for same value of i keep moving it
+        if (nums[i] == nums[i - 1] && i > 0)
+        {
+            continue;
+            // ie do i++
+        }
+        int j = i + 1;
+        int k = n - 1;
+
+        while (j < k)
+        {
+            if (nums[i] + nums[j] + nums[k] == 0)
+            {
+                int prevJ = nums[j];
+                int prevK = nums[k];
+                ans.push_back({nums[i], nums[j], nums[k]});
+                while (nums[j] == prevJ && j<k)
+                {
+                    j++;
+                }
+                while (nums[k] == prevK && j<k)
+                {
+                    k--;
+                }
+            }
+            else if (nums[i] + nums[j] + nums[k] < 0)
+            {
+                // lesser than 0 so move j
+                int prevJ = nums[j];
+                while (nums[j] == prevJ && j<k)
+                {
+                    j++;
+                }
+            }
+            else if (nums[i] + nums[j] + nums[k] > 0)
+            {
+                // greater than 0 so movek
+                int prevK = nums[k];
+                while (nums[k] == prevK && j<k)
+                {
+                    k--;
+                }
+            }
+        }
+    }
+
+    cout << "The 3 different numbers such that when added=0 are: \n";
+    for (auto row : ans)
+    {
+        for (auto it : row)
+        {
+            cout << it << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    vector<int> arr = {-1, 0, 1, 2, -1, -4};
+    // Output: [[-1,-1,2],[-1,0,1]]
+
+    // threeSum_brute(arr);
+    // threeSum_better(arr);
+    threeSum_optimal(arr);
+
+    return 0;
+}
--- Leetcode/229MajorityElementII.cpp
+++ Leetcode/229MajorityElementII.cpp
@@ -1,7 +1,7 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-// BRUTE FORCE: TIME:O(n^2)
+// BRUTE FORCE: TIME:O(n^2)         SPACE:LIST WITH MAX OF 2 ELEMENTS-O(2) SO CONST SPACE
 void majorityElement_brute(vector<int> &arr)
 {
     int n = arr.size();
@@ -18,7 +18,13 @@ void majorityElement_brute(vector<int> &arr)
                 count++;
             }
         }
-        if (count > limit)
+
+        if (ans.size() == 2)
+        {
+            break;
+            // as list can contain max of 2 elements
+        }
+        else if (count > limit)
         {
             ans.push_back(arr[i]);
         }
@@ -31,7 +37,7 @@ void majorityElement_brute(vector<int> &arr)
     }
 }
 
-// BETTER->HASHING: TIME:O(n*1) best case
+// BETTER->HASHING: TIME:O(n*1) best case   but worst:O(n*n)        SPACE- EXTRA MAP: O(n)
 void majorityElement_better(vector<int> &arr)
 {
     int n = arr.size();
@@ -41,13 +47,15 @@ void majorityElement_better(vector<int> &arr)
     unordered_map<int, int> hashMap; // element,count
     for (int i = 0; i < n; i++)
     {
-        hashMap[arr[i]]++;
-    }
-    for (auto it : hashMap)
-    {
-        if (it.second > limit)
+        hashMap[arr[i]] += 1;
+        if (hashMap[arr[i]] == limit + 1) // occurs min of n/3+1 times
         {
-            ans.push_back(it.first);
+            ans.push_back(arr[i]);
+        }
+        if (ans.size() == 2)
+        {
+            break;
+            // as list can contain max of 2 elements
         }
     }
 
@@ -58,13 +66,67 @@ void majorityElement_better(vector<int> &arr)
     }
 }
 
-// OPIMAL: TIME:O()
+// OPIMAL: TIME:O(n)         MOORE'S VOTING ALGO
 void majorityElement_optimal(vector<int> &arr)
 {
     int n = arr.size();
     int limit = int(n / 3);
     cout << "Limit is " << limit << endl;
     vector<int> ans;
+    int element1;
+    int count1 = 0;
+    int element2;
+    int count2 = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (count1 == 0 && arr[i] != element2)
+        {
+            element1 = arr[i];
+            count1 = 1;
+        }
+        else if (count2 == 0 && arr[i] != element1)
+        {
+            element2 = arr[i];
+            count2 = 1;
+        }
+        else if (arr[i] == element1)
+        {
+            count1++;
+        }
+        else if (arr[i] == element2)
+        {
+            count2++;
+        }
+        else
+        {
+            count1--;
+            count2--;
+        }
+    }
+
+    // verify
+    int count1Verify = 0;
+    int count2Verify = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == element1)
+        {
+            count1Verify++;
+        }
+        else if (arr[i] == element2)
+        {
+            count2Verify++;
+        }
+    }
+    if (count1Verify > limit)
+    {
+        ans.push_back(element1);
+    }
+    if (count2Verify > limit)
+    {
+        ans.push_back(element2);
+    }
 
     cout << "Elements that appear more than n/3 times are: \n";
     for (auto it : ans)
