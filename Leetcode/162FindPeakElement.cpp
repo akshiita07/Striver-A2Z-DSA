1bb02655e464ba92b88cd39752bb658870f9a3f7
Akshita Pathak <pathakshita07@gmail.com>
2024-07-31 14:33:53 +0530

    Binary search easy

--- Leetcode/162FindPeakElement.cpp
+++ Leetcode/162FindPeakElement.cpp
@@ -1,4 +1,10 @@
-int n = nums.size();
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+// TIME: O(logn)
+int findPeakElement(vector<int> &nums)
+{
+    int n = nums.size();
     // if only single element in array
     if(n==1){
         return 0;   //index return karna hai
@@ -6,18 +12,18 @@ int n = nums.size();
     // array range: -♾️[]-♾️
     // check for first element
     if(nums[0]>nums[1] && nums[0]>INT_MIN){
-        return nums[0];
+        return 0;   //index return karna hai
     }
     // check for last element
     if(nums[n-1]>nums[n-2] && nums[n-1]>INT_MIN){
-        return nums[n-1];
+        return n-1;   //index return karna hai
     }
     int low=1;
     int high=n-2;
     while(low<=high){
         int mid=(low+high)/2;
         if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
-            return nums[mid];
+            return mid;   //index return karna hai
         }else{
             if(nums[mid]<nums[mid+1]){
                 low=mid+1;
@@ -26,4 +32,12 @@ int n = nums.size();
             }
         }
     }
-    return -1;
+    return -1;  //if no peak element
+}
+
+int main()
+{
+    vector<int> nums = {1, 2, 1, 3, 5, 6, 4};
+    cout << "\nThe peak element is: " << findPeakElement(nums);
+    return 0;
+}
--- Leetcode/540SingleElementinaSortedArray.cpp
+++ Leetcode/540SingleElementinaSortedArray.cpp
@@ -1,11 +1,66 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
+// BRUTE FORCE: TIME-O(n)
+int brute_force(vector<int> &nums)
+{
+    int n = nums.size();
+    // if only 1 element present in array:
+    if (n == 1)
+    {
+        return nums[0];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        // first element
+        if (i == 0)
+        {
+            if (nums[i] != nums[i + 1])
+            {
+                return nums[i];
+            }
+        }
+        // last element
+        else if (i == (n - 1))
+        {
+            if (nums[i] != nums[i - 1])
+            {
+                return nums[i];
+            }
+        }
+        // any other element
+        else
+        {
+            if (nums[i] != nums[i - 1] && nums[i] != nums[i + 1])
+            {
+                // neithe on side is same
+                return nums[i];
+            }
+        }
+    }
+}
+
+// BINARY SEARCH: TIME-O(logn)
 int singleNonDuplicate(vector<int> &nums)
 {
     int n = nums.size();
-    int low = 0;
-    int high = n - 1;
+    // if only 1 element present in array:
+    if (n == 1)
+    {
+        return nums[0];
+    }
+    // check for first element
+    if (nums[0] != nums[1])
+    {
+        return nums[0];
+    }
+    // check for last element
+    if (nums[n-1] != nums[n - 2])
+    {
+        return nums[n-1];
+    }
+    int low = 1;        //from second elemnt
+    int high = n - 2;   //from second last element
     while (low <= high)
     {
         int mid = (low + high) / 2;
@@ -17,14 +72,14 @@ int singleNonDuplicate(vector<int> &nums)
         }
         else
         {
-            // odd no of digits remaining: search in that half
-            if ((mid + 1) % 2 == 0)
-            {
-                low = mid + 1;
-            }
-            else if ((high - mid + 1) % 2 == 0)
-            {
-                high = mid - 1;
+            // if mid is at odd index & its prev elem is same
+            // if mid is at even index & its next elem is same
+            if((mid%2!=0 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1])){
+                // presnt at prev index
+                // eliminate left half
+                low=mid+1;
+            }else{
+                high=mid-1;
             }
         }
     }
@@ -34,6 +89,7 @@ int singleNonDuplicate(vector<int> &nums)
 int main()
 {
     vector<int> nums = {3, 3, 7, 7, 10, 11, 11};
+    // cout << "\nBRUTE FORCE: The no that occurs exactly once in array is: " << brute_force(nums);
     cout << "\nThe no that occurs exactly once in array is: " << singleNonDuplicate(nums);
 
     return 0;
