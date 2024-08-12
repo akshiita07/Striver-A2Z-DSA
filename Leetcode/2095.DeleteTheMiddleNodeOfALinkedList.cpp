22195a9fc65f7ad3368a3ada22effe32c4477cc3
Akshita Pathak <pathakshita07@gmail.com>
2024-08-12 09:49:10 +0530

    Linked Lists Tortoise & Hare Approach

---
+++ Leetcode/2095.DeleteTheMiddleNodeOfALinkedList.cpp
@@ -0,0 +1,113 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+};
+
+Node *convertArrToLL(int arr[], int n)
+{
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+    for (int i = 1; i < n; i++)
+    {
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+void traverse(Node *head)
+{
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+// USING OPTIMIZED TORTOISE & HARE APPROACH:
+Node *deleteElement(Node *head, Node *nodeToDelete)
+{
+    int c = 0;
+    Node *temp = head;
+    Node *prev = NULL;
+    while (temp != nullptr)
+    {
+        if (temp == nodeToDelete)
+        {
+            if (c == 0)
+            {
+                // delete 1st index
+                head = head->next;
+                temp->next = nullptr;
+                delete temp;
+                return head;
+            }
+            prev->next = temp->next;
+            delete temp;
+            return head;
+        }
+        c++;
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+Node *deleteMiddle(Node *head)
+{
+    Node *slow = head;
+    Node *fast = head;
+    Node *newLL = NULL;
+    // for odd length: fast->next=nullptr
+    // for even length: fast-=nullptr
+    while (fast != nullptr && fast->next != nullptr)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+    newLL = deleteElement(head, slow); // middle element
+    return newLL;
+}
+
+int main()
+{
+    // odd length list
+    int arr1[] = {1, 3, 4, 7, 1, 2, 6};
+    int n1 = sizeof(arr1) / sizeof(arr1[0]);
+    Node *node1 = convertArrToLL(arr1, n1);
+    cout << "\nTraversing LL1: ";
+    traverse(node1);
+    deleteMiddle(node1);
+    cout << "\nAfter deleting middle element of LL1: ";
+    traverse(node1);
+
+    // even length list
+    int arr2[] = {1, 2, 3, 4};
+    int n2 = sizeof(arr2) / sizeof(arr2[0]);
+    Node *node2 = convertArrToLL(arr2, n2);
+    cout << "\nTraversing LL2: ";
+    traverse(node2);
+    deleteMiddle(node2);
+    cout << "\nAfter deleting middle element of LL2: ";
+    traverse(node2);
+
+    return 0;
+}
---
+++ LinkedList/13.FindLengthOfCyclicLoop.cpp
@@ -0,0 +1,69 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+// If there is no cycle, return 0, otherwise return the length of the cycle.
+
+// Using hash map
+// TIME: O(n*2*1)-> traversing LL & unordered map so 2*O(1) e 2 opeartions of map->find/value
+// SPACE: O(n)->storing all nodes
+int brute_force(Node *head)
+{
+    unordered_map<Node *, int> hashMap;
+    // store node & this i
+    Node *temp = head;
+    int i = 1;
+    while (temp != nullptr)
+    {
+        // first check if already exists in map
+        if (hashMap.find(temp) != hashMap.end())
+        {
+            // found so cycle exists
+            int value = hashMap[temp];
+            return (i - value);
+        }
+        // else add it
+        hashMap[temp] = i;
+        i++;
+        temp = temp->next; // move temp!! 😭
+    }
+    return 0; // no cycle exists so return 0
+}
+
+// OPTIMLA USING TORTOISE & HARE APPROACH
+// TIME: O(n+n)=O(2n)
+// SPACE: O(1)
+int findLength(Node *slow, Node *fast)
+{
+    // TIME: O(length of loop) =O(n)
+    int count = 1;
+    fast = fast->next;
+    while (fast != slow)
+    {
+        count++;
+        fast = fast->next;
+    }
+    return count;
+}
+int lengthOfLoop(Node *head)
+{
+    Node *slow = head;
+    Node *fast = head;
+    while (fast != nullptr && fast->next != nullptr)
+    {
+        // TIME:O(n)
+
+        // move slow by 1
+        slow = slow->next;
+        // move fast by 2
+        fast = fast->next->next;
+
+        // if they collide then cycle exists
+        if (slow == fast)
+        {
+            // cycle exists
+            // now take a counter & move fast to find length
+            return findLength(slow, fast);
+        }
+    }
+    return 0; // no cycle exists so return 0
+}
