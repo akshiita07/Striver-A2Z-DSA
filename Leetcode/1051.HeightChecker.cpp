66c32a753fb3afe4b7ecae66d329498e6b98e57d
Akshita Pathak <pathakshita07@gmail.com>
2024-08-15 22:58:44 +0530

    LeetcodeExplore

---
+++ Leetcode/1051.HeightChecker.cpp
@@ -0,0 +1,19 @@
+﻿class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        // get actual sorted order of heights:
+        vector<int> actualOrder=heights;
+        sort(actualOrder.begin(),actualOrder.end());
+        // compare which indices r wrong
+        int count=0;
+        for(int i=0;i<heights.size();i++){
+            if(heights[i]==actualOrder[i]){
+                // correct index
+                continue;
+            }else{
+                count++;
+            }
+        }
+        return count;
+    }
+};
---
+++ Leetcode/383.RansomNote.cpp
@@ -0,0 +1,29 @@
+﻿class Solution
+{
+public:
+    bool canConstruct(string ransomNote, string magazine)
+    {
+        // get all letters of maganize:
+        unordered_map<char, int> hashMap;
+        for (int i = 0; i < magazine.length(); i++)
+        {
+            hashMap[magazine[i]] += 1;
+        }
+        // get all letters of ransom notes:
+        for (int i = 0; i < ransomNote.length(); i++)
+        {
+            // now check if magaz contains all ransom
+            if (hashMap[ransomNote[i]] > 0)
+            {
+
+                hashMap[ransomNote[i]] -= 1; // reduce its value also
+            }
+            else
+            {
+                // its value is 0:
+                return false;
+            }
+        }
+        return true;
+    }
+};
