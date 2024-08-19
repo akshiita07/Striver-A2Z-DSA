1a57cfe4d03c4295fbcc702d2f8a9cc9790a3f1b
Akshita Pathak <pathakshita07@gmail.com>
2024-08-19 12:15:40 +0530

    Monotonic stack

--- Leetcode/84.LargestRectangleInHistogram.cpp
+++ Leetcode/84.LargestRectangleInHistogram.cpp
@@ -1,14 +1,144 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int largestRectangleArea(vector<int>& heights) {
-        
+vector<int> nextSmallerElement(vector<int> &heights)
+{
+    int n = heights.size();
+    vector<int> nse(n);
+    stack<int> st;
+    // store indices (not elements)
+    // loop from back
+    for (int i = n - 1; i >= 0; i--)
+    {
+        while (st.empty() == false && heights[st.top()] >= heights[i])
+        {
+            st.pop();
+        }
+        if (st.empty())
+        {
+            nse[i] = n; // till max index n
+        }
+        else
+        {
+            nse[i] = st.top();
+        }
+        st.push(i);
     }
+    return nse;
+}
+vector<int> previousSmallerElement(vector<int> &heights)
+{
+    int n = heights.size();
+    vector<int> pse(n);
+    stack<int> st;
+    // store indices (not elements)
+    for (int i = 0; i < n; i++)
+    {
+        while (st.empty() == false && heights[st.top()] >= heights[i])
+        {
+            st.pop();
+        }
+        if (st.empty())
+        {
+            pse[i] = -1;
+        }
+        else
+        {
+            pse[i] = st.top();
+        }
+        st.push(i);
+    }
+    return pse;
+}
+// TIME: O(2n)->for nse + O(2n)->for pse + O(n)->for loop == O(5n)
+// SPACE: O(n)->nse stack + O(n)->pse stack + O(2n)->2vectors for nse&pse == O(4n)
+int bruteforce(vector<int> &heights)
+{
+    // maintain list of all possible max areas
+    vector<int> areas;
+    int n = heights.size();
+    if (n == 1)
+    {
+        // only 1 element
+        return heights[0];
+    }
+    vector<int> pse = previousSmallerElement(heights);
+    vector<int> nse = nextSmallerElement(heights);
+    for (int i = 0; i < n; i++)
+    {
+        // find pse & nse ka index
+        int prevSmallIndex = pse[i];
+        int nextSmallIndex = nse[i];
+        int width = nextSmallIndex - prevSmallIndex - 1;
+        areas.push_back(width * heights[i]);
+    }
+    // return max of these all areas
+    return *max_element(areas.begin(), areas.end());
+}
+
+// TIME: O(n)->for loop + O(n)->popping atmax n elements == O(2n)
+// SPACE: O(n)->stack
+int largestRectangleArea(vector<int> &heights)
+{
+    // find nse & pse in single pass:
+    int maxArea = 0;
+    stack<int> st; // stores index
+    int n = heights.size();
+    if (n == 1)
+    {
+        // only 1 element
+        return heights[0];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        while (st.empty() == false && heights[st.top()] > heights[i])
+        {
+            int elem = st.top();
+            st.pop();
+            int nse = i;
+            int pse;
+            if (st.empty() == false)
+            {
+                pse = st.top();
+            }
+            else
+            {
+                pse = -1;
+            }
+            maxArea = max(maxArea, ((nse - pse - 1) * heights[elem]));
+        }
+        // check if pse exists in stack
+        // add new element as PSE exists
+        st.push(i);
+    }
+    // for remaining elements
+    while (st.empty() == false)
+    {
+        int elem = st.top();
+        st.pop();
+        int nse = n; // when nse does not exists
+        int pse;
+        if (st.empty() == false)
+        {
+            pse = st.top();
+        }
+        else
+        {
+            pse = -1;
+        }
+        maxArea = max(maxArea, ((nse - pse - 1) * heights[elem]));
+    }
+    return maxArea;
+}
 
 int main()
 {
-    vector<int> heights={2,1,5,6,2,3};
-    cout<<"\nThe largest area of rectangle is: "<<largestRectangleArea(heights);
+    vector<int> heights1 = {2, 1, 5, 6, 2, 3};
+    cout << "\n1:BRUTE FORCE--> The largest area of rectangle is: " << bruteforce(heights1);
+    cout << "\n1:The largest area of rectangle is: " << largestRectangleArea(heights1);
+    vector<int> heights2 = {0, 9};
+    cout << "\n2:BRUTE FORCE--> The largest area of rectangle is: " << bruteforce(heights2);
+    cout << "\n2:The largest area of rectangle is: " << largestRectangleArea(heights2);
 
     return 0;
 }
---
+++ Leetcode/85.MaximalRectangle.cpp
@@ -0,0 +1,79 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int findAreaOfHistogram(vector<int> &matrix)
+{
+    int maxArea = 0;
+    int n = matrix.size();
+    stack<int> st;
+    for (int i = 0; i < n; i++)
+    {
+        while (!st.empty() && matrix[st.top()] > matrix[i])
+        {
+            int elem = matrix[st.top()];
+            st.pop();
+            int nse = i;
+            int pse = st.empty() ? -1 : st.top();
+            maxArea = max(maxArea, (nse - pse - 1) * elem);
+        }
+        st.push(i);
+    }
+    while (!st.empty())
+    {
+        int elem = matrix[st.top()];
+        st.pop();
+        int nse = n;
+        int pse = st.empty() ? -1 : st.top();
+        maxArea = max(maxArea, (nse - pse - 1) * elem);
+    }
+    return maxArea;
+}
+// TIME: O(n*m)->for prefix sum + O(n)*(2n)->for histogram  ==  O(n2)
+// SPACE: O(n*m)->prefix sum + O(n)->for stack n histogram
+int maximalRectangle(vector<vector<char>> &matrix)
+{
+    // for each row of matrix consider histogram bars that start with '1'
+    // do not consideer bars starting from 0
+    int rows = matrix.size();     // n
+    int colms = matrix[0].size(); // m
+    int maxArea = 0;
+    vector<vector<int>> prefixSumMatrix(rows, vector<int>(colms,0));
+
+    // find top to bottom sums for prefix sum
+    for (int i = 0; i < colms; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < rows; j++)
+        {
+            int elem;
+            if (matrix[j][i] == '0')
+            {
+                elem = 0;
+            }
+            else if (matrix[j][i] == '1')
+            {
+                elem = 1;
+            }
+            sum += elem;
+            if (matrix[j][i] == '0')
+            {
+                sum = 0;
+            }
+            prefixSumMatrix[j][i] = sum;
+        }
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
+        maxArea = max(maxArea, findAreaOfHistogram(prefixSumMatrix[i]));
+    }
+    return maxArea;
+}
+
+int main()
+{
+    vector<vector<char>> matrix = {{'1', '0', '1', '0', '0'}, {'1', '0', '1', '1', '1'}, {'1', '1', '1', '1', '1'}, {'1', '0', '0', '1', '0'}};
+    cout << "\nThe maximum area of continuous 1s is: " << maximalRectangle(matrix);
+
+    return 0;
+}
