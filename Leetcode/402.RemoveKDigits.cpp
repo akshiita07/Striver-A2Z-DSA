c054a83f1df390a45b98173ffd678b3a1af65e5f
Akshita Pathak <pathakshita07@gmail.com>
2024-08-20 10:08:23 +0530

    Monotonic Stack

---
+++ Leetcode/402.RemoveKDigits.cpp
@@ -0,0 +1,66 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+// TIME: O(n)->for loop + O(k)->to pop while loop + O(n)->add from stack to string + O(n)->reversing number + O(n)->removing 0s from end == O(3n+k)
+// SPACE: O(n)->for stack   + O(n)->to return string as answer == O(2n)
+string removeKdigits(string num, int k)
+{
+    stack<char> st; // stores characters
+    int n = num.length();
+
+    if (k == n)
+    {
+        // all digits will be removed
+        return "0";
+    }
+
+    // traverse thru string
+    for (int i = 0; i < n; i++)
+    {
+        while (st.empty() == false && k > 0 && (st.top() - '0') > (num[i]) - '0')
+        {
+            // bigger then pop & reduce k
+            st.pop();
+            k--;
+        }
+        st.push(num[i]);
+    }
+
+    // check if k is still left then remove elements from RIGHT
+    while (k > 0)
+    {
+        st.pop(); // remove top elements from stack!
+        k--;
+    }
+
+    // add elements from stack into string
+    string ans = "";
+    while (!st.empty())
+    {
+        ans.push_back(st.top());
+        st.pop();
+    }
+    // check for zeroes at end:
+    while (!ans.empty() && ans.back() == '0')
+    {
+        ans.pop_back();
+    }
+
+    // reverse string:
+    reverse(ans.begin(), ans.end());
+// check if still ans is empty eg:"10" then all elem removed so return "0"
+    if(ans.empty()){
+            return "0";
+        }
+
+    return ans;
+}
+
+int main()
+{
+    string num = "1432219";
+    int k = 3;
+    cout << "\nThe SMALLEST possible number when " << k << " digits r removed: " << removeKdigits(num, k);
+
+    return 0;
+}
---
+++ Leetcode/901.OnlineStockSpan.cpp
@@ -0,0 +1,74 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+// TIME: O(n)->for loop traversing backwards
+// SPACE: O(n)->for list
+class StockSpanner_BruteForce
+{
+public:
+    vector<int> list;
+    StockSpanner_BruteForce()
+    {
+    }
+
+    int next(int price)
+    {
+        list.push_back(price);
+        // returns span
+        int count = 1; // not counting current element:
+        for (int i = list.size() - 2; i >= 0; i--)
+        {
+            // backward traversal:
+            if (list[i] <= price)
+            {
+                count++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return count;
+    }
+};
+
+// OPTIMIZED::
+// TIME: O(2n)->to compute PGE
+// SPACE: O(n)->stack
+class StockSpanner
+{
+public:
+    stack<pair<int, int>> st;
+    int index;
+    StockSpanner()
+    {
+        index = -1;
+        st.clear();
+    }
+
+    int next(int price)
+    {
+        index++;
+        // returns span
+
+        // find index of previous greater element for each new price added
+        // ANS count=current index-PGE index
+        int pgi;
+        while (st.empty() == false && st.top().first <= price)
+        {
+            st.pop();
+        }
+        if (st.empty())
+        {
+            pgi = -1; // no prev greater elem index
+        }
+        else
+        {
+            pgi = st.top().second;
+        }
+        st.push({price,index});
+
+        int count = index - pgi;
+        return count;
+    }
+};
