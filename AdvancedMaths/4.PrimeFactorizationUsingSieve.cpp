1b5d4353738e3d4bec947d0acf4f82179a827aae
Akshita Pathak <pathakshita07@gmail.com>
2024-08-23 10:23:38 +0530

    Maths Complete

---
+++ AdvancedMaths/4.PrimeFactorizationUsingSieve.cpp
@@ -0,0 +1,87 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+// TIME: O(sqrt(n))
+// SPACE: O(n)->to return ans
+void brute_force(int n)
+{
+    vector<int> ans;
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            // ie divisble so keep on dividing
+            while (n % i == 0)
+            {
+                ans.push_back(i);
+                n = n / i;
+            }
+        }
+    }
+    if (n != 1)
+    {
+        ans.push_back(n);
+    }
+    cout << "\nThe prime factors of " << n << " are: ";
+    for (auto it : ans)
+    {
+        cout << it << " ";
+    }
+}
+
+// SEIVE USED IS: SPF- smallest prime factor
+vector<int> sieve(int n)
+{
+    vector<int> spf(n + 1);
+    // from 2 to n store indices a values
+    for (int i = 2; i <= n; i++)
+    {
+        spf[i] = i;
+    }
+    // now loop till sqrt(n)
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        // check if it is prime
+        if (spf[i] == i)
+        {
+            // is prime:
+            // update factors:
+            for (int j = i * i; j <= n; j = j + i)
+            {
+                // check if factors r not previously updated
+                if (spf[j] == j)
+                {
+                    // now update it
+                    spf[j] = i;
+                }
+            }
+        }
+    }
+    return spf;
+}
+
+void findPrimeFactors(int n)
+{
+    vector<int> spf=sieve(n);
+    vector<int> ans;
+    while (n != 1)
+    {
+        ans.push_back(spf[n]);
+        n = n / spf[n];
+    }
+    // return ans;
+    cout << "\nThe prime factors of " << n << " are: ";
+    for (auto it : ans)
+    {
+        cout << it << " ";
+    }
+}
+
+int main()
+{
+    int n = 60;
+    // brute_force(n);
+    findPrimeFactors(n);
+
+    return 0;
+}
---
+++ Leetcode/2523.ClosestPrimeNumbersInRange.cpp
@@ -0,0 +1,80 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> getSeive(int left, int right)
+{
+    vector<int> ans;
+    vector<int> primeArr(right + 1);
+    for (int i = 2; i <= right; i++)
+    {
+        primeArr[i] = 1;
+    }
+    for (int i = 2; i <= sqrt(right); i++)
+    {
+        if (primeArr[i] == 1)
+        {
+            // is prime so remove multiples:
+            for (int j = i * i; j <= right; j = j + i)
+            {
+                primeArr[j] = 0;
+            }
+        }
+    }
+
+    for (int i = left; i <= right; i++)
+    {
+        if (primeArr[i] == 1)
+        {
+            ans.push_back(i);
+        }
+    }
+    return ans;
+}
+
+void closestPrimes(int left, int right)
+{
+    vector<int> ans;
+
+    // check all prime nos between left & right
+    vector<int> primeFactors = getSeive(left, right);
+    cout << "\nThe prime numbers between " << left << " & right are= ";
+    for (auto it : primeFactors)
+    {
+        cout << it << " ";
+    }
+
+    // if only 1/none primeFactors exists then return [-1,-1]
+    if (primeFactors.size() <= 1)
+    {
+        ans = {-1, -1};
+    }
+    else if (primeFactors.size() > 1)
+    {
+        // find min range in these primeFactors:  11 13 17 19
+        int minDiff = INT_MAX;
+        for (int i = 0; i < primeFactors.size() - 1; i++)
+        {
+            int diff = primeFactors[i + 1] - primeFactors[i];
+            if (diff < minDiff)
+            {
+                minDiff = diff;
+                ans = {primeFactors[i], primeFactors[i + 1]};
+            }
+        }
+    }
+
+    cout << "\nThe prime numbers between " << left << " & right with min range= ";
+    for (auto it : ans)
+    {
+        cout << it << " ";
+    }
+}
+
+int main()
+{
+    int left = 10;
+    int right = 19;
+    closestPrimes(left, right);
+
+    return 0;
+}
