5b64bb97c61044693cf7e6caa12a86318d6a81cc
Akshita Pathak <pathakshita07@gmail.com>
2024-08-27 23:06:37 +0530

    Binary Tree

---
+++ BinaryTree/1.ConstructionOfBinaryTree.cpp
@@ -0,0 +1,26 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node* left;
+        node* right;
+
+        // constructor to initialise:
+        node(int val){
+            data=val;
+            left=nullptr;
+            right=nullptr;
+        }
+};
+
+int main()
+{
+    node* root=new node(1);
+    root->left=new node(2);
+    root->right=new node(3);
+    root->left->right=new node(5);
+
+    return 0;
+}
---
+++ BinaryTree/2.NumberOfNodesAt_ith_Level.cpp
@@ -0,0 +1,15 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int countNodes(int i)
+{
+    return pow(2, i - 1);
+}
+
+int main()
+{
+    int i = 5;
+    cout << "The no of nodes at " << i << " level are: " << countNodes(i);
+
+    return 0;
+}
