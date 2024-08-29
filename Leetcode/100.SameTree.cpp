46bb2b87d92e200f46d6d3e6827982e26face99c
Akshita Pathak <pathakshita07@gmail.com>
2024-08-29 19:03:01 +0530

    Binary Tree

---
+++ Leetcode/100.SameTree.cpp
@@ -0,0 +1,61 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+class TreeNode
+{
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    // constructor to initialise:
+    TreeNode(int d)
+    {
+        val = d;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+// root left right
+
+// are tree 1 and tree2 identical??
+bool isSameTree(TreeNode *p, TreeNode *q)
+{
+    // traverse the tree & check any 1 traversal of both trees must be same
+    if (p == NULL && q == NULL)
+    {
+        return true;
+    }
+    if (p == NULL || q == NULL)
+    {
+        return false;
+    }
+    if ((p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right))
+    {
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+    TreeNode *root1 = new TreeNode(1);
+    root1->left = new TreeNode(2);
+    root1->right = new TreeNode(3);
+
+    TreeNode *root2 = new TreeNode(1);
+    root2->left = new TreeNode(2);
+    root2->right = new TreeNode(3);
+
+    cout << "\nIs tree1 and tree2 same: " << isSameTree(root1, root2);
+
+    TreeNode *root3 = new TreeNode(1);
+    root3->left = new TreeNode(2);
+    root3->right = new TreeNode(3);
+    root3->left->left = new TreeNode(4);
+
+    cout << "\nIs tree1 and tree3 same: " << isSameTree(root1, root3);
+
+    return 0;
+}
---
+++ Leetcode/543.DiameterOfBinaryTree.cpp
@@ -0,0 +1,82 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+class TreeNode
+{
+public:
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+
+    // constructor to initialise:
+    TreeNode(int val)
+    {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+int findHeight(TreeNode *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    int l = findHeight(root->left);
+    int r = findHeight(root->right);
+    return 1 + max(l, r);
+}
+
+// TIME: O(n^2)->finding height+ moving to next node
+// SPACE: O()
+int bruteforce(TreeNode *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    // length of the longest path between any two nodes in a tree
+    //  The length of a path between two nodes is represented by the number of edges between them.
+
+    int maxi = INT_MIN;
+    // go find height of left + height of right->maxm
+    int lHeight = findHeight(root->left);
+    cout << "\nMaxm height for left subtree is: " << lHeight;
+    int rHeight = findHeight(root->right);
+    cout << "\nMaxm height for right subtree is: " << rHeight;
+    int l = bruteforce(root->left);
+    int r = bruteforce(root->right);
+    maxi = max(max(l, r), (lHeight + rHeight));
+    return maxi;
+}
+
+int heightTree(TreeNode *root,int dia)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    int l = heightTree(root->left,dia);
+    int r = heightTree(root->right,dia);
+    dia=max(dia,l+r);
+    return 1 + max(l, r);
+}
+
+int diameterOfBinaryTree(TreeNode *root){
+    int dia=0;
+    return heightTree(root,dia);    
+}
+
+int main()
+{
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    // cout << "\nThe length of diameter of tree is: " << bruteforce(root);
+    cout << "\nThe length of diameter of tree is: " << diameterOfBinaryTree(root);
+
+    return 0;
+}
