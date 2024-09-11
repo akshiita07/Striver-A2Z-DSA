db6960f9ac28d289cf8ccb94338de6ca6a0c1e65
Akshita Pathak <pathakshita07@gmail.com>
2024-09-11 19:15:45 +0530

    Arrays

---
+++ Leetcode/448.FindAllNumbersDisappearedInAnArray.cpp
@@ -0,0 +1,18 @@
+﻿class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> doNotAppear;
+        int n = nums.size();
+        unordered_map<int, int> mpp;
+        for (int i = 0; i < n; i++) {
+            mpp[nums[i]]++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if(mpp[i]==0){
+                doNotAppear.push_back(i);
+            }
+        }
+        return doNotAppear;
+    }
+};
