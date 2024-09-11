1a654796c8bce6a12152014c78ec7f2a124c457e
Akshita Pathak <pathakshita07@gmail.com>
2024-09-11 07:35:04 +0530

    Leetcode POTD

---
+++ Leetcode/1394.FindLuckyIntegerInAnArray.cpp
@@ -0,0 +1,17 @@
+﻿class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        // lucky intger frequency==value
+        unordered_map<int,int> mpp;
+        int maxLargest=-1;
+        for(int i=0;i<arr.size();i++){
+            mpp[arr[i]]++;
+        }
+        for(auto it:mpp){
+            if(it.second==it.first){
+                maxLargest=max(it.first,maxLargest);
+            }
+        }
+        return maxLargest;
+    }
+};
---
+++ Leetcode/387.FirstUniqueCharacterInAString.cpp
@@ -0,0 +1,16 @@
+﻿class Solution {
+public:
+    int firstUniqChar(string s) {
+        // give element that appears only once
+        unordered_map<char,int> mpp;
+        for(int i=0;i<s.length();i++){
+            mpp[s[i]]++;
+        }
+        for(int i=0;i<s.length();i++){
+            if(mpp[s[i]]==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
