30277f1760cc0bb77f38bb271499bc2ca51f8850
Akshita Pathak <pathakshita07@gmail.com>
2024-09-12 20:43:18 +0530

    leetcode

---
+++ Leetcode/1748.SumOfUniqueElements.cpp
@@ -0,0 +1,16 @@
+﻿class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        unordered_map<int,int> hashMap;
+        for(auto it:nums){
+            hashMap[it]++;
+        }
+        int sum=0;
+        for(auto it:hashMap){
+            if(it.second==1){
+                sum+=it.first;
+            }
+        }
+        return sum;
+    }
+};
---
+++ Leetcode/1941.CheckIfAllCharactersHaveEqualNumberOfOccurrences.cpp
@@ -0,0 +1,26 @@
+﻿class Solution {
+public:
+    bool areOccurrencesEqual(string s) {
+        // if all charcaters appear same times then good string:
+        vector<int> hashMap(26, 0);
+        for (int i = 0; i < s.length(); i++) {
+            hashMap[s[i] - 'a']++;
+        }
+        int freq;
+        for (auto it : hashMap) {
+            if (it > 0) {
+                freq = it;
+            }
+        }
+        bool goodString=true;
+        for (auto it : hashMap) {
+            if (it > 0 && it != freq) {
+                goodString=false;
+            }
+        }
+        if(goodString){
+            return true;
+        }
+        return false;
+    }
+};
