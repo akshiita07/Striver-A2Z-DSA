4139e93900c1e96397b6552975e2170996514bb7
Akshita Pathak <pathakshita07@gmail.com>
2024-09-13 09:37:01 +0530

    Leetcode POTD

---
+++ Leetcode/1310.XORQueriesOfASubarray.cpp
@@ -0,0 +1,38 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> xorQueries(vector<int> &arr, vector<vector<int>> &queries)
+{
+    vector<int> ans;
+
+    // first loop thru all queries
+    for (int i = 0; i < queries.size(); i++)
+    {
+        // select a particular query
+        vector<int> query = queries[i];
+        // loop from start to end of this first query
+        int XOR = 0;        //xor with no leads number itself
+        for (int j = query[0]; j <= query[1]; j++)
+        {
+            // i will iterate from 0 to 1 in arr to find xor
+            XOR = XOR ^ arr[j];
+        }
+        ans.push_back(XOR);
+    }
+
+    cout << "\nThe XOR of queries yields: ";
+    for (auto it : ans)
+    {
+        cout << it << " ";
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr = {1, 3, 4, 8};
+    vector<vector<int>> queries = {{0, 1}, {1, 2}, {0, 3}, {3, 3}};
+    // Output: [2,7,14,8]
+    xorQueries(arr, queries);
+    return 0;
+}
---
+++ Leetcode/682.BaseballGame.cpp
@@ -0,0 +1,36 @@
+﻿class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        stack<string> st;
+
+        for (int i = 0; i < operations.size(); i++) {
+            if (operations[i]=="C"){
+                st.pop();
+            }else if (operations[i]=="D"){
+                int elem=stoi(st.top());
+                elem=elem*2;
+                st.push(to_string(elem));
+            }else if (operations[i]=="+"){
+                //sum of the previous two scores & then push in stack
+                int elem1=stoi(st.top());
+                st.pop();
+                int elem2=stoi(st.top());
+                st.pop();
+                int sum=elem1+elem2;
+                st.push(to_string(elem2));
+                st.push(to_string(elem1));
+                st.push(to_string(sum));
+            }else{
+                // store integer
+                st.push(operations[i]);
+            }
+        }
+        // return sum of all elem of stack
+        int sum=0;
+        while(!st.empty()){
+            sum+=stoi(st.top());
+            st.pop();
+        }
+        return sum;
+    }
+};
