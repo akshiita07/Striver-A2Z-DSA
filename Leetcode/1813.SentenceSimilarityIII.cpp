f6b19a1d362b67ba077c43c2c0d40d8f6c33864c
Akshita Pathak <pathakshita07@gmail.com>
2024-10-06 17:44:31 +0530

    leetcode

---
+++ Leetcode/1813.SentenceSimilarityIII.cpp
@@ -0,0 +1,69 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+bool areSentencesSimilar(string sentence1, string sentence2)
+{
+    // vector<string> word1=split(sentence1);
+    // vector<string> word2=split(sentence2);
+
+    if (sentence1.size() > sentence2.size())
+    {
+        swap(sentence1, sentence2);
+    }
+
+    // split sentence into words:
+    istringstream iss1(sentence1);
+    istringstream iss2(sentence2);
+    vector<string> word1;
+    vector<string> word2;
+    string word;
+
+    while (iss1 >> word)
+    {
+        word1.push_back(word);
+    }
+    while (iss2 >> word)
+    {
+        word2.push_back(word);
+    }
+
+    if (word1.size() > word2.size())
+    {
+        swap(word1, word2);
+    }
+
+    // 2 pointers
+    int i = 0;
+    int j = 0;
+
+    int n1 = word1.size();
+    int n2 = word2.size();
+
+    // Compare from the beginning
+    while (i < n1 && word1[i] == word2[i])
+    {
+        i++;
+    }
+
+    // Compare from the end
+    while (j < n1 && word1[n1 - j - 1] == word2[n2 - j - 1])
+    {
+        j++;
+    }
+
+    // Check if there is an unmatched part that prevents similarity
+    return i + j >= n1;
+}
+
+int main()
+{
+    string sentence1 = "My name is Haley";
+    string sentence2 = "My Haley";
+    cout << "\nIs sentence 1 and 2 similar: " << areSentencesSimilar(sentence1, sentence2);
+
+    string sent1 = "of";
+    string sent2 = "A lot of words";
+    cout << "\nIs sent 1 and 2 similar: " << areSentencesSimilar(sent1, sent2);
+
+    return 0;
+}
--- Leetcode/567.PermutationInString.cpp
+++ Leetcode/567.PermutationInString.cpp
@@ -3,15 +3,23 @@ using namespace std;
 
 bool checkInclusion(string s1, string s2)
 {
-    int n1 = s1.size(), n2 = s2.size();
-    if (n1 > n2)
-        return false;
-
-    vector<int> count1(26, 0), count2(26, 0);
-    for (int i = 0; i < n1; ++i)
+    int n1 = s1.size();
+    int n2 = s2.size();
+    // if 2nd word does not contain enough digits as in n1 then exit
+    if (s2.length() < s1.length())
     {
-        ++count1[s1[i] - 'a'];
-        ++count2[s2[i] - 'a'];
+        return false;
+    }
+
+    // 2 hash maps to mainatin count of letters
+    vector<int> count1(26, 0);
+    vector<int> count2(26, 0);
+
+    // add digits of string in hash map 1 and 2
+    for (int i = 0; i < n1; i++)
+    {
+        count1[s1[i] - 'a']++;
+        count2[s2[i] - 'a']++;
     }
 
     for (int i = n1; i < n2; ++i)
@@ -27,6 +35,9 @@ bool checkInclusion(string s1, string s2)
 
 int main()
 {
+    string s1 = "ab";
+    string s2 = "eidbaooo";
+    cout << "\nDoes s2 contain permutation of s1: " << checkInclusion(s1, s2);
 
     return 0;
 }
