25993b81df4fbab9065e5260f0651c4d18541f06
Akshita Pathak <pathakshita07@gmail.com>
2024-10-08 09:23:49 +0530

    leetcode potd

---
+++ Leetcode/1963.MinimumNumberOfSwapsToMakeTheStringBalanced.cpp
@@ -0,0 +1,39 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int minSwaps(string s)
+{
+    int balance = 0;
+    int unmatched = 0;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '[')
+        {
+            // opening bracket so increment balance
+            balance++;
+        }
+        else if (s[i] == ']')
+        {
+            // closing bracket so decrement balance
+            balance--;
+        }
+        // if balance goes below 0:
+        if (balance < 0)
+        {
+            // increment unmatches
+            unmatched++;
+            // reset balance back to 0
+            balance = 0;
+        }
+    }
+    // min no of swaps:
+    return (unmatched + 1) / 2;
+}
+
+int main()
+{
+    string s = "][][";
+    cout << "\nMin no of swaps reqd to make string balanced are: " << minSwaps(s);
+
+    return 0;
+}
