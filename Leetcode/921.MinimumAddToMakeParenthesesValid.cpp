df35f0438a70e2ee8641a5852a6884a92d9c653b
Akshita Pathak <pathakshita07@gmail.com>
2024-10-09 07:41:24 +0530

    leetcode potd

---
+++ Leetcode/921.MinimumAddToMakeParenthesesValid.cpp
@@ -0,0 +1,46 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int minAddToMakeValid(string s)
+{
+    if (s.length() == 0)
+    {
+        // empty string
+        return 0; // always valid
+    }
+    stack<int> st;
+    int bracketReqd = 0;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '(')
+        {
+            // opening bracket then push in stack
+            st.push(s[i]);
+        }
+        else if (s[i] == ')')
+        {
+            // closing brckt then pop from stack
+            if (st.empty())
+            {
+                // if no opening bracket present then:
+                st.push('(');
+                bracketReqd++;
+            }
+            st.pop();
+        }
+    }
+    if (!st.empty())
+    {
+        // add remaining opening brackets
+        bracketReqd += st.size();
+    }
+    return bracketReqd;
+}
+
+int main()
+{
+    string s = "())";
+    cout << "\nMin no of paranthesis reqd to make string valid are: " << minAddToMakeValid(s);
+
+    return 0;
+}
