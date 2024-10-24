a0a9d31380d8534c40e58b17b13c03f1f9a5f1d6
Akshita Pathak <pathakshita07@gmail.com>
2024-10-24 09:15:40 +0530

    leetcode potd

---
+++ Leetcode/951.FlipEquivalentBinaryTrees.cpp
@@ -0,0 +1,26 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+bool flipEquiv(TreeNode *root1, TreeNode *root2)
+{
+    // If both trees are empty, they are flip equivalent
+    if (!root1 && !root2)
+    {
+        return true;
+    }
+    // If only one is empty or values are different, they are not flip equivalent
+    if (!root1 || !root2 || root1->val != root2->val)
+    {
+
+        return false;
+    }
+
+    // Check if the trees are equivalent either by not flipping or by flipping
+    return ((flipEquiv(root1->left, root2->left) && flipEquiv(root1->right, root2->right)) || (flipEquiv(root1->left, root2->right) && flipEquiv(root1->right, root2->left)));
+}
+
+int main()
+{
+
+    return 0;
+}
