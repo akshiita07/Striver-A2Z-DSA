24ae437d5ea991717e47b4d4e45de981bed7cafe
Akshita Pathak <pathakshita07@gmail.com>
2024-10-29 10:29:26 +0530

    potd

--- Leetcode/2501.LongestSquareStreakInAnArray.cpp
+++ Leetcode/2501.LongestSquareStreakInAnArray.cpp
@@ -1,17 +1,23 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
+// square streak:
+// 1. length of the subsequence is at least 2  (2 or more: length)
+// 2. after sorting the subsequence, each element (except the first element) is the square of the previous number
+// Return the length of the longest square streak
+
 int longestSquareStreak(vector<int> &nums)
 {
+    // first sort nums
     sort(nums.begin(), nums.end());
 
     unordered_set<long long> numSet(nums.begin(), nums.end());
 
     int longestStreak = 0;
 
-    for (auto it : nums)
+    for (int i = 0; i < nums.size(); i++)
     {
-        long long current = it;
+        long long current = nums[i];
         int streak = 0;
 
         // Check if we can form a square streak with 'current'
@@ -19,33 +25,29 @@ int longestSquareStreak(vector<int> &nums)
         {
             streak++;
             current *= current; // Square the number
-            // Stop if current exceeds our bounds to avoid overflow or invalid values
-            if (current > 1e5){
-                break;
-
-            }
         }
 
-        // Update longest streak found
+        // Update longest streak found when length is atleast 2
         if (streak >= 2)
         {
             longestStreak = max(longestStreak, streak);
         }
     }
 
+    // when length is atleast 2
     if (longestStreak >= 2)
     {
         return longestStreak;
     }
-    else
-    {
-        return -1;
-    }
+    // if there is no square streak so return -1
     return -1;
 }
 
 int main()
 {
+    // given integer array:
+    vector<int> nums = {4, 3, 6, 16, 8, 2};
+    cout << "\nLength of longest square streak in nums is: " << longestSquareStreak(nums);
 
     return 0;
 }
---
+++ Leetcode/2684.MaximumNumberOfMovesInAGrid.cpp
@@ -0,0 +1,52 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int maxMoves(vector<vector<int>> &grid)
+{
+    // m x n matrix grid
+    // From (row, col), you can move to: (row - 1, col + 1):upper right diagonal, (row, col + 1):next right elements and (row + 1, col + 1):lower right diagonal
+    // value of the cell you move to, should be strictly bigger than the value of the current cell.
+    // Return the maximum number of moves that you can perform.
+    int m = grid.size();    // rows
+    int n = grid[0].size(); // colms
+    int max_moves = 0;      // track no of moves thatcan be performed
+
+    // dp[i][j] := the maximum number of moves you can perform from (i, j)
+    // initialized to zeros
+    //  Each cell dp[i][j] will store the maximum number of moves possible
+    vector<vector<int>> dp(m, vector<int>(n));
+
+    for (int j = n - 2; j >= 0; j--)
+        for (int i = 0; i < m; i++)
+        {
+            // next right element
+            if (grid[i][j + 1] > grid[i][j])
+            {
+                dp[i][j] = 1 + dp[i][j + 1];
+            }
+            // upper right diagonal
+            if (i > 0 && grid[i - 1][j + 1] > grid[i][j])
+            {
+                dp[i][j] = max(dp[i][j], 1 + dp[i - 1][j + 1]);
+            }
+            // lower right diagonal
+            if (i + 1 < m && grid[i + 1][j + 1] > grid[i][j])
+            {
+                dp[i][j] = max(dp[i][j], 1 + dp[i + 1][j + 1]);
+            }
+        }
+
+    for (int i = 0; i < m; ++i)
+    {
+        max_moves = max(max_moves, dp[i][0]);
+    }
+
+    return max_moves;
+}
+
+int main()
+{
+    vector<vector<int>> grid = {{2, 4, 3, 5}, {5, 4, 9, 3}, {3, 4, 2, 11}, {10, 9, 13, 15}};
+    cout << "\nMaxm no of moves possible are " << maxMoves(grid);
+    return 0;
+}
