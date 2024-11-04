be834b5e903caceff1e37a1e6133c7793232ea9a
Akshita Pathak <pathakshita07@gmail.com>
2024-11-04 13:03:40 +0530

    potd

---
+++ Leetcode/3163.StringCompressionIII.cpp
@@ -0,0 +1,34 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+string compressedString(string word)
+{
+    // empty string
+    string comp = "";
+    int cnt = 0;
+
+    while (cnt < word.length())
+    {
+        // Remove a maximum length prefix of word made of a single character c repeating at most 9 times.
+        // Append the length of the prefix followed by c to comp.
+        char currentChar = word[cnt];
+        int noOfTimesRepeat = 0;
+        while (cnt < word.length() && word[cnt] == currentChar && noOfTimesRepeat < 9)
+        {
+            // repeating word
+            noOfTimesRepeat++;
+            cnt++;
+        }
+
+        comp += to_string(noOfTimesRepeat) + currentChar;
+    }
+    return comp;
+}
+
+int main()
+{
+    string word = "abcde";
+    cout << "\nComp string is: " << compressedString(word);
+
+    return 0;
+}
