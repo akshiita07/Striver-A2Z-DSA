e7fead43caf6627e90fc7c6783926faac00a1603
Akshita Pathak <pathakshita07@gmail.com>
2024-11-05 09:19:40 +0530

    potd

---
+++ Leetcode/2914.MinimumNumberOfChangesToMakeBinaryStringBeautiful.cpp
@@ -0,0 +1,25 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int minChanges(string s)
+{
+    // binary string s with even length
+    // beautiful string conditions..partition string into substrings: 1. Each substring has an even length.
+    // 2. Each substring contains only 1's or only 0's.
+    // Return the minimum number of changes required to make the string s beautiful
+    int n=s.length();
+    int changes=0;
+    for(int i=0;i<n;i=i+2){
+        if(s[i]!=s[i+1]){
+            changes++;
+        }
+    }
+    return changes;
+}
+
+int main()
+{
+    string s = "1001";
+    cout << "\nMin no of changes reqd to make string beautiful are: " << minChanges(s);
+    return 0;
+}
