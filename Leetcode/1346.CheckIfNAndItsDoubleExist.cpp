f25819188b9665c383e50bc6ae2a8ed636c81918
Akshita Pathak <pathakshita07@gmail.com>
2024-12-01 06:40:26 +0530

    potd

---
+++ Leetcode/1346.CheckIfNAndItsDoubleExist.cpp
@@ -0,0 +1,17 @@
+﻿class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        // pehle duplicate then number
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j){
+                    if(arr[i]==2*arr[j]){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
