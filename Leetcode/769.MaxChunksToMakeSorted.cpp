e4868452c8da1c0a6c9f3d272d8e3ae44d7ac138
Akshita Pathak <pathakshita07@gmail.com>
2024-12-19 11:01:55 +0530

    potd

---
+++ Leetcode/769.MaxChunksToMakeSorted.cpp
@@ -0,0 +1,25 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int maxChunksToSorted(vector<int> &arr)
+{
+    // split array into max no of chunks st on sorting each chunk and concatenating we get orig sorted array
+    int n = arr.size();
+    int maxValInChunk=0;
+    int ans=0;
+    for(int i=0;i<n;i++){
+        maxValInChunk=max(maxValInChunk,arr[i]);
+        if(maxValInChunk==i){
+            ans++;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr = {1, 0, 2, 3, 4};
+    cout << "\nMax chunks possible are: " << maxChunksToSorted(arr);
+
+    return 0;
+}
