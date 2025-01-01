101d14b6132b92ff560e088b78b27d33a62c1780
Akshita Pathak <pathakshita07@gmail.com>
2025-01-01 10:58:17 +0530

    first potd of 2025!

---
+++ Leetcode/1422.MaximumScoreAfterSplittingAString.cpp
@@ -0,0 +1,35 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int maxScore(string s)
+{
+    // no of zeroes in left + no of ones in right substring
+    int n=s.length();
+    int totalOnes=0;
+    for(auto it:s){
+        if(it=='1'){
+            totalOnes++;
+        }
+    }
+    int ans=0;
+    int leftZeros=0;
+    int rightOnes=totalOnes;
+    for(int i=0;i<n-1;i++){
+        if(s[i]=='0'){
+            leftZeros++;
+        }else{
+            rightOnes--;
+        }
+        int currentScore=leftZeros+rightOnes;
+        ans=max(ans,currentScore);
+    }
+    return ans;
+}
+
+int main()
+{
+    string s = "011101";
+    cout << "\nMax score is: " << maxScore(s);
+
+    return 0;
+}
