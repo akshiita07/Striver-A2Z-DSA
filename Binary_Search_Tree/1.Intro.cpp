fe21a859bd3d8a14e4f9ee6ce14b98bdaa9e124b
Akshita Pathak <pathakshita07@gmail.com>
2025-01-03 11:38:09 +0530

    potd + bin search tree

---
+++ Binary_Search_Tree/1.Intro.cpp
@@ -0,0 +1,62 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    // constructor to initialise:
+    node(int val)
+    {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+// left<root<right
+
+// time: height of tree: O(log2n)
+node *search(node *root, int key)
+{
+    while (root != null && root->data != key)
+    {
+        if (root->data > key)
+        {
+            return search(root->left, key);
+        }
+        else
+        {
+            return search(root->right, key);
+        }
+    }
+    return root;
+}
+
+bool isValidBSTInorder(vector<int> &arr) {
+    for (int i = 1; i < arr.size(); i++) {
+        if (arr[i] <= arr[i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    node *root = new node(8);
+    root->left = new node(5);
+    root->right = new node(12);
+    root->left->left = new node(4);
+    root->left->right = new node(7);
+    root->left->right->left = new node(6);
+    root->right->left = new node(10);
+    root->right->right = new node(14);
+    root->right->right->left = new node(13);
+    searchNode(10);
+
+    return 0;
+}
---
+++ Leetcode/2270.NumberOfWaysToSplitArray.cpp
@@ -0,0 +1,58 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int brute_force(vector<int> &nums)
+{
+    int n = nums.size();
+    int numOfValidSplits = 0;
+    // sum of first i + 1 elements >= sum of last n - i - 1 elements.
+    // There is at least one element to the right of i : 0 <= i < n - 1
+    int leftSum = 0;
+    int rightSum = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        leftSum += nums[i];
+        rightSum = 0;
+        for (int j = i + 1; j < n; j++)
+        {
+            rightSum += nums[j];
+        }
+        if (leftSum >= rightSum)
+        {
+            numOfValidSplits++;
+        }
+    }
+    return numOfValidSplits;
+}
+
+int waysToSplitArray(vector<int> &nums)
+{
+    int n = nums.size();
+    int numOfValidSplits = 0;
+    // sum of first i + 1 elements >= sum of last n - i - 1 elements.
+    // There is at least one element to the right of i : 0 <= i < n - 1
+    long long totalSum = 0;
+    for (auto it : nums)
+    {
+        totalSum += it;
+    }
+    long long leftSum = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        leftSum += nums[i];
+        long long rightSum = totalSum - leftSum;
+        if (leftSum >= rightSum)
+        {
+            numOfValidSplits++;
+        }
+    }
+    return numOfValidSplits;
+}
+
+int main()
+{
+    vector<int> nums = {10, 4, -8, 7};
+    cout << "\nBrute: No of valid splits are: " << brute_force(nums);
+    cout << "\nNo of valid splits are: " << waysToSplitArray(nums);
+    return 0;
+}
