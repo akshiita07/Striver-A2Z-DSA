561f0ff6e3d70be9658b73ed256db3671f709b1e
Akshita Pathak <pathakshita07@gmail.com>
2025-01-07 11:55:18 +0530

    potd

---
+++ Leetcode/1408.StringMatchingInAnArray.cpp
@@ -0,0 +1,35 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> stringMatching(vector<string> &words)
+{
+    //  return all strings in words that is a substring of another word
+    int n = words.size();
+    vector<string> ans;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            // check if word[i] is a subtring of word[j]
+            // npos checks if a substring exists or not in a string
+            if (i != j && words[j].find(words[i]) != string::npos)
+            {
+                ans.push_back(words[i]);
+                break;
+            }
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<string> words = {"mass", "as", "hero", "superhero"};
+    vector<string> ans = stringMatching(words);
+    for (auto it : ans)
+    {
+        cout << it << " ";
+    }
+
+    return 0;
+}
