dcb31155dc3846bdeb6f4a489ab84dd6e62201eb
Akshita Pathak <pathakshita07@gmail.com>
2025-01-09 14:21:07 +0530

    potd

---
+++ Leetcode/2185.CountingWordsWithAGivenPrefix.cpp
@@ -0,0 +1,26 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int prefixCount(vector<string> &words, string pref)
+{
+    // Return the number of strings in words that contain pref as a prefix
+    int count = 0;
+    int len = pref.length();
+    for (int i = 0; i < words.size(); i++)
+    {
+        if (words[i].substr(0, len) == pref)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    vector<string> words = {"pay", "attention", "practice", "attend"};
+    string pref = "at";
+    cout << "\nThe no of strings with given prefix are: " << prefixCount(words, pref);
+
+    return 0;
+}
