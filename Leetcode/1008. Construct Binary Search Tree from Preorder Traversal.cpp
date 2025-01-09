1483c931bf0ba660cfbad8335ce94c030dda4241
Akshita Pathak <pathakshita07@gmail.com>
2025-01-09 15:06:59 +0530

    binary search tree

---
+++ Leetcode/1008. Construct Binary Search Tree from Preorder Traversal.cpp
@@ -0,0 +1,103 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// time:O(n*n): n nodes & for every ode determine left right & stuff
+TreeNode *insert(TreeNode *root, int val)
+{
+    if (root == NULL)
+    {
+        return new TreeNode(val);
+    }
+    if (val < root->val)
+    {
+        root->left = insert(root->left, val);
+    }
+    else
+    {
+        root->right = insert(root->right, val);
+    }
+    return root;
+}
+
+TreeNode *brute_force(vector<int> &preorder)
+{
+    TreeNode *root = NULL;
+    for (auto it : preorder)
+    {
+        root = insert(root, it);
+    }
+    return root;
+}
+
+// time: O(nlogn):sorting + O(n):to create bst from preorder & inorder
+// space: O(n):to store inorder tree
+TreeNode *constructFromPreIn(vector<int> &preorder, vector<int> &inorder, int &i, int inStart, int inEnd, unordered_map<int, int> &inMap)
+{
+    if (inStart > inEnd)
+    {
+        return nullptr;
+    }
+
+    int rootVal = preorder[i++];
+    TreeNode *root = new TreeNode(rootVal);
+
+    int inIndex = inMap[rootVal];
+
+    root->left = constructFromPreIn(preorder, inorder, i, inStart, inIndex - 1, inMap);
+    root->right = constructFromPreIn(preorder, inorder, i, inIndex + 1, inEnd, inMap);
+
+    return root;
+}
+
+TreeNode *better(vector<int> &preorder)
+{
+    // inorder traversal is always sorted
+    // unique bst from preorder & inorder:
+    // preorder: root left right
+    // inorder: left root right
+    vector<int> inorder = preorder;
+    sort(inorder.begin(), inorder.end());
+
+    unordered_map<int, int> inMap;
+    for (int i = 0; i < inorder.size(); i++)
+    {
+        inMap[inorder[i]] = i;
+    }
+
+    int i = 0;
+    return constructFromPreIn(preorder, inorder, i, 0, inorder.size() - 1, inMap);
+}
+
+// time: O(3*n)=O(n) :worst time is visiting a node 3 times
+TreeNode *buildTree(vector<int> &preorder, int &i, int upper_bound)
+{
+    // if no more elemnets to be inserted in bst OR if exceeded bound:
+    if (i == preorder.size() || preorder[i] > upper_bound)
+    {
+        return NULL;
+    }
+    TreeNode *root = new TreeNode(preorder[i++]);
+    root->left = buildTree(preorder, i, root->val);
+    root->right = buildTree(preorder, i, upper_bound);
+    return root;
+}
+TreeNode *bstFromPreorder(vector<int> &preorder)
+{
+    // preorder: root left right
+    // each node belongs within range ..start from [INT_MIN,INT_MAX] for left node range: [INT_MIN,root_val] and  for right node range: [root_val,INT_MAX]
+    // only upper bound reqd (lower bound not reqd)
+
+    // pointer:
+    int i = 0;
+    return buildTree(preorder, i, INT_MAX);
+}
---
+++ Leetcode/235.LowestCommonAncestorOfABinarySearchTree.cpp
@@ -0,0 +1,40 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
+public:
+    // time: O(log2n) and space:O(1)
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        // the FIRST INTERSECTION POINT from the given nodes to the root of tree
+        if (root == NULL)
+        {
+            return NULL;
+        }
+        // find value of root
+        int current = root->val;
+        // if both nodes lie on right
+        if (current < p->val && current < q->val)
+        {
+            // move to right:
+            return lowestCommonAncestor(root->right, p, q);
+        }
+        // if both nodes lie on left
+        if (current > p->val && current > q->val)
+        {
+            // move to left:
+            return lowestCommonAncestor(root->left, p, q);
+        }
+        // cannot determine:
+        return root;
+    }
+};
