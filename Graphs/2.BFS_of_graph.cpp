1803c21b874eb635a973ce5e6628528e6d848cf6
Akshita Pathak <pathakshita07@gmail.com>
2025-01-16 19:27:54 +0530

    graphs

---
+++ Graphs/2.BFS_of_graph.cpp
@@ -0,0 +1,77 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+// go equivalent edges in the breadth
+// traversal changes depending upon the starting node
+// implementation: queue
+
+// V:no of nodes
+// adjacency list of edges given
+
+// Time: O(n):queue + O(2*e):total degree for each node == O(n)+O(2e)
+// Space: O(n)queue + O(n)vector for bfs ans + O(n):visited array = O(3n)
+void bfs(int V, vector<vector<int>> adjacencyList)
+{
+    vector<int> visited(n, 0);
+    queue<int> qu;
+    qu.push(0);
+    // mark first node as visited:
+    visited[0] = 1;
+
+    // until qu is empty:
+    while (!qu.empty())
+    {
+        // take out first queue element
+        int node = qu.front();
+        cout << qu.front(); // bfs traversal   (or add in a list)
+        // pop from queue:
+        qu.pop();
+        // traverse neighbours of node:
+        for (auto it : adjacencyList[node])
+        {
+            // add them to queue if NOT already visited:
+            if (!visited[it])
+            {
+                qu.push(it);
+            }
+            // add them to visited array:
+            visited[it] = 1;
+        }
+    }
+}
+
+vector<int> bfsOfGraph(vector<vector<int>> &adj)
+{
+    // Code here
+    int n = adj.size();
+    vector<int> visited(n, 0);
+    queue<int> qu;
+    qu.push(0);
+    // mark first node as visited:
+    visited[0] = 1;
+
+    vector<int> bfs;
+    // until qu is empty:
+    while (!qu.empty())
+    {
+        // take out first queue element
+        int node = qu.front();
+        // cout << qu.front(); // bfs traversal   (or add in a list)
+        // pop from queue:
+        bfs.push_back(node);
+        qu.pop();
+        // traverse neighbours of node:
+        for (auto it : adj[node])
+        {
+            // add them to queue if NOT already visited:
+            if (!visited[it])
+            {
+                qu.push(it);
+            }
+            // add them to visited array:
+            visited[it] = 1;
+        }
+    }
+
+    return bfs;
+}
---
+++ Graphs/3.DFS_of_Graph.cpp
@@ -0,0 +1,53 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+// TIME: O(n)+O(2*e)
+// SPACE: O(n):adjacency list +  O(n):visited + O(n):recursion stack space = O(3n)
+
+// go depth wise: RECURSION
+void dfs_traversal(int node, vector<vector<int>> &adjacencyList, vector<int> &visited)
+{
+    visited[node] = 1;
+    cout << node; // or store in a list DFS TRAVERSAL
+    for (auto it : adjacencyList[node])
+    {
+        // go depth wise:
+        if (!visited[it])
+        {
+            dfs_traversal(it, adjacencyList, visited);
+        }
+    }
+}
+void dfs(int V, vector<vector<int>> adjacencyList)
+{
+    vector<int> visited(n, 0);
+    // mark first node as visited:
+    visited[0] = 1;
+    dfs_traversal(0, adjacencyLis, visited);
+}
+
+// actual:
+void dfs_traversal(int node, vector<int> &dfs, vector<vector<int>> &adj, vector<int> &visited)
+{
+    visited[node] = 1;
+    // cout << node; // or store in a list DFS TRAVERSAL
+    dfs.push_back(node);
+    for (auto it : adj[node])
+    {
+        // go depth wise:
+        if (!visited[it])
+        {
+            dfs_traversal(it, dfs, adj, visited);
+        }
+    }
+}
+vector<int> dfsOfGraph(vector<vector<int>> &adj)
+{
+    int n = adj.size();
+    vector<int> dfs;
+    vector<int> visited(n, 0);
+    // mark first node as visited:
+    visited[0] = 1;
+    dfs_traversal(0, dfs, adj, visited);
+    return dfs;
+}
