d86360431cd4968ed4db6112ba5e13ce0e18885d
Akshita Pathak <pathakshita07@gmail.com>
2025-02-04 09:07:54 +0530

    POTD

---
+++ Leetcode/1800.Maximum_Ascending_Subarray_Sum.cpp
@@ -0,0 +1,35 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int maxAscendingSum(vector<int> &nums)
+{
+    // return the maximum possible sum of an ascending subarray in nums
+    int n = nums.size();
+    int currentSum = nums[0];
+    int maxSum = INT_MIN;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (nums[i] > nums[i - 1])
+        {
+            currentSum += nums[i];
+        }
+        else
+        {
+            maxSum = max(maxSum, currentSum);
+            currentSum = nums[i]; // restart
+        }
+        cout << "\nCurr sum= " << currentSum;
+        cout << "\nMax sum= " << maxSum;
+    }
+    maxSum = max(maxSum, currentSum); // for last subarray
+    return maxSum;
+}
+
+int main()
+{
+    vector<int> nums = {12, 17, 15, 13, 10, 11, 12}; // 33
+    cout << "\nMaximum possible sum of an ascending subarray= " << maxAscendingSum(nums);
+
+    return 0;
+}
