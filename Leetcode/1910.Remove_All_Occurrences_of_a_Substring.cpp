db4519417491ffc23fc2993297dd0bf50b8e4e8e
Akshita Pathak <pathakshita07@gmail.com>
2025-02-11 07:37:41 +0530

    potd

---
+++ Leetcode/1910.Remove_All_Occurrences_of_a_Substring.cpp
@@ -0,0 +1,52 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+string removeOccurrences(string s, string part)
+{
+    // return string s after removing all occurrences of substring part from left to right:
+    // push characters into stack
+    stack<int> stk;
+    int n = part.length();
+    for (auto it : s)
+    {
+        // push character in stacK
+        stk.push(it);
+        // check stack top if it matches part:
+        if (stk.size() >= n)
+        {
+            string patternCheck = "";
+            for (int i = 0; i < n; i++)
+            {
+                patternCheck += stk.top();
+                stk.pop();
+            }
+            // check if match:
+            reverse(patternCheck.begin(), patternCheck.end());
+            if (patternCheck != part)
+            {
+                // if pattern does not matches then push characters back into stack:
+                for (int i = 0; i < n; i++)
+                {
+                    stk.push(patternCheck[i]);
+                }
+            }
+        }
+    }
+    // remaining string s is in stack:
+    string ans = "";
+    while (!stk.empty())
+    {
+        ans += stk.top();
+        stk.pop();
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+int main()
+{
+    string s = "daabcbaabcbc";
+    string part = "abc";
+    cout << "\nString s after removing all occurrences of substring part= " << removeOccurrences(s, part);
+    return 0;
+}
