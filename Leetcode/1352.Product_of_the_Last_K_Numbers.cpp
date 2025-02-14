0dcba7a9057384e52ac9e2b0ef10d0ac62a6ca46
Akshita Pathak <pathakshita07@gmail.com>
2025-02-14 07:22:44 +0530

    potd

---
+++ Leetcode/1352.Product_of_the_Last_K_Numbers.cpp
@@ -0,0 +1,32 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> stream;
+ProductOfNumbers()
+{
+    // initialise object with empty stream
+}
+
+void add(int num)
+{
+    // Appends the integer num to the stream
+    stream.push_back(num);
+}
+
+int getProduct(int k)
+{
+    // Returns the product of the last k numbers in the current list
+    int n = stream.size();
+    int product = 1;
+    for (int i = n - 1; i >= n - k; i--)
+    {
+        product *= stream[i];
+    }
+    return product;
+}
+
+int main()
+{
+
+    return 0;
+}
