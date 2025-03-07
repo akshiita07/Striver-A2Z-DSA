5eccc72d3b889d61df97b49ac76ddc19eb22bd9b
Akshita Pathak <pathakshita07@gmail.com>
2025-03-07 07:06:29 +0530

    potd

---
+++ Leetcode/1550. Three Consecutive Odds.cpp
@@ -0,0 +1,23 @@
+﻿class Solution
+{
+public:
+    bool threeConsecutiveOdds(vector<int> &arr)
+    {
+        int n = arr.size();
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] % 2 != 0)
+            {
+                // it is odd check next 2:
+                if (i < n && (i + 1) < n && (i + 2) < n)
+                {
+                    if (arr[i + 1] % 2 != 0 && arr[i + 2] % 2 != 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
---
+++ Leetcode/3232. Find if Digit Game Can Be Won.cpp
@@ -0,0 +1,31 @@
+﻿class Solution
+{
+public:
+    bool canAliceWin(vector<int> &nums)
+    {
+        // check all single digit numbers & put in one array & then check sum:
+        vector<int> alice;
+        vector<int> bob;
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] <= 9)
+            {
+                // single digit
+                alice.push_back(nums[i]);
+            }
+            else
+            {
+                // double digit
+                bob.push_back(nums[i]);
+            }
+        }
+        int aliceSum = accumulate(alice.begin(), alice.end(), 0);
+        int bobSum = accumulate(bob.begin(), bob.end(), 0);
+        if (aliceSum > bobSum || bobSum > aliceSum)
+        {
+            return true;
+        }
+        return false;
+    }
+};
