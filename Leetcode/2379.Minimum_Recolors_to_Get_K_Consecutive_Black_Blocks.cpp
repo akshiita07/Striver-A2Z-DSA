b2b6bf87da70563ac07f80cafd589ca47201f9ad
Akshita Pathak <pathakshita07@gmail.com>
2025-03-08 07:10:02 +0530

    8march potd

---
+++ Leetcode/2379.Minimum_Recolors_to_Get_K_Consecutive_Black_Blocks.cpp
@@ -0,0 +1,41 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int minimumRecolors(string blocks, int k)
+{
+    int n = blocks.length(); // contains W:white & B:black
+    // k- desired number of consecutive black block
+    // Return the minimum number of operations to recolor a white to black block in 1operation to get atleast one series of k consecutive black boxes
+    int whites = 0;
+    for (int i = 0; i < k; i++)
+    {
+        if (blocks[i] == 'W')
+        {
+            whites++;
+        }
+    }
+    int ans = whites;
+    for (int i = k; i < n; i++)
+    {
+        if (blocks[i] == 'W')
+        {
+            whites++;
+        }
+        if (blocks[i - k] == 'W')
+        {
+            // remove leftmost
+            whites--;
+        }
+        ans = min(ans, whites);
+    }
+    return ans;
+}
+
+int main()
+{
+    string blocks = "WBBWWBBWBW";
+    int k = 7;
+    cout << "\nMinimum number of operations= " << minimumRecolors(blocks, k);
+
+    return 0;
+}
