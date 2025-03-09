a7b37e39afd50530af01876cb7947a0645a5f288
Akshita Pathak <pathakshita07@gmail.com>
2025-03-09 06:49:16 +0530

    potd

---
+++ Leetcode/3208.Alternating_Groups_II.cpp
@@ -0,0 +1,37 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int numberOfAlternatingGroups(vector<int> &colors, int k)
+{
+    // colors[i] == 0: red & colors[i] == 1: blue
+    // check from each index if alternating colors present then count++
+    int n = colors.size();
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        bool isAlternating = true;
+        // check kth length window:
+        for (int j = 1; j < k; j++)
+        {
+            // If two adjacent colors are the same, it breaks the loop because the sequence is not alternating
+            if (colors[(i + j) % n] == colors[(i + j - 1) % n])
+            {
+                isAlternating = false;
+                break;
+            }
+        }
+        if (isAlternating)
+        {
+            ans++;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> colors = {0, 1, 0, 1, 0};
+    int k = 2;
+    cout << "\nAlt colors= " << numberOfAlternatingGroups(colors, k) << endl;
+    return 0;
+}
