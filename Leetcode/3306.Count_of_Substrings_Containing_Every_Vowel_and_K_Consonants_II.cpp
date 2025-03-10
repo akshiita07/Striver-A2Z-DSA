a4f9cf5824f01e61c0dab5ad96ff8c0c7fa2cfcf
Akshita Pathak <pathakshita07@gmail.com>
2025-03-10 07:43:19 +0530

    potd

---
+++ Leetcode/3306.Count_of_Substrings_Containing_Every_Vowel_and_K_Consonants_II.cpp
@@ -0,0 +1,95 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+long long brute_force(string word, int k)
+{
+    // Return total number of substrings of word that contain every vowel ('a', 'e', 'i', 'o', and 'u') at least once & exactly k consonants.
+    int n = word.size();
+    long long ans = 0;
+    // create set of vowels:
+    unordered_set<char> vowelsSet = {'a', 'e', 'i', 'o', 'u'};
+    // take sliding windoe from l to r:
+    for (int l = 0; l < n; l++)
+    {
+        // create map for counting vowels:
+        unordered_map<char, int> hashMap;
+        long long consonants = 0;
+        for (int r = l; r < n; r++)
+        {
+            char currentChar = word[r];
+            // add character in window if vowel:
+            if (vowelsSet.find(currentChar) != vowelsSet.end())
+            {
+                hashMap[currentChar]++;
+            }
+            else
+            {
+                consonants++;
+            }
+            if (consonants > k)
+            {
+                break;
+            }
+            // shrink window if no of consonants > k:
+            if (hashMap.size() == 5 && consonants == k)
+            {
+                ans++;
+            }
+        }
+    }
+    return ans;
+}
+
+long long countOfSubstrings(string word, int k)
+{
+    auto countSubstrings = [&](int k) -> long long
+    {
+        long long ans = 0;
+        int l = 0;
+        long long consonants = 0;
+        unordered_map<char, int> hashMap;
+        auto vowel = [&](char c) -> bool
+        {
+            return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+        };
+        for (char c : word)
+        {
+            if (vowel(c))
+            {
+                hashMap[c]++;
+            }
+            else
+            {
+                ++consonants;
+            }
+            while (consonants >= k && hashMap.size() == 5)
+            {
+                char leftMostChar = word[l++];
+                if (vowel(leftMostChar))
+                {
+                    if (--hashMap[leftMostChar] == 0)
+                    {
+                        hashMap.erase(leftMostChar);
+                    }
+                }
+                else
+                {
+                    --consonants;
+                }
+            }
+            ans += l;
+        }
+        return ans;
+    };
+
+    return countSubstrings(k) - countSubstrings(k + 1);
+}
+
+int main()
+{
+    string word = "ieaouqqieaouqq";
+    int k = 1;
+    cout << "\n total number of substrings= " << countOfSubstrings(word, k);
+
+    return 0;
+}
--- Leetcode/tempCodeRunnerFile.cpp
+++ Leetcode/tempCodeRunnerFile.cpp
@@ -1,1 +1,67 @@
-waterTrapped = 0
+#include <bits/stdc++.h>
+using namespace std;
+
+long long countOfSubstrings(string word, int k)
+{
+    // lambda fnc:
+    auto countSubstrings = [&](int k) -> long long
+    {
+        // Return total number of substrings of word that contain every vowel ('a', 'e', 'i', 'o', and 'u') at least once & exactly k consonants.
+        int n = word.size();
+        long long ans = 0;
+        // create set of vowels:
+        unordered_set<char> vowelsSet = {'a', 'e', 'i', 'o', 'u'};
+        // create map for counting vowels:
+        unordered_map<char, int> hashMap;
+        long long consonants = 0;
+        // take sliding windoe from l to r:
+        int l = 0;
+        for (int r = 0; r < n; r++)
+        {
+            char currentChar = word[r];
+            // add character in window if vowel:
+            if (vowelsSet.find(currentChar) != vowelsSet.end())
+            {
+                hashMap[word[r]]++;
+            }
+            else
+            {
+                consonants++;
+            }
+
+            // chrink window if no of consonants > k:
+            while (consonants > k)
+            {
+                char leftMostChar = word[l];
+                if (vowelsSet.find(word[l]) != vowelsSet.end())
+                {
+                    hashMap[leftMostChar]--;
+                    if (hashMap[leftMostChar] == 0)
+                    {
+                        hashMap.erase(leftMostChar);
+                    }
+                }
+                else
+                {
+                    consonants--;
+                }
+                l++; // shrink window
+            }
+            // if all vowels & exactly k consonants are present in window:
+            if (hashMap.size() == 5 && consonants == k)
+            {
+                ans += l;
+            }
+        }
+    };
+    return countSubstrings(k) - countSubstrings(k + 1);
+}
+
+int main()
+{
+    string word = "ieaouqqieaouqq";
+    int k = 1;
+    cout << "\n total number of substrings= " << countOfSubstrings(word, k);
+
+    return 0;
+}
