f45bad22089fe6762af4a34f4d625d77079ca631
Akshita Pathak <pathakshita07@gmail.com>
2025-03-14 07:44:54 +0530

    potd

---
+++ Leetcode/2226.Maximum_Candies_Allocated_to_K_Children.cpp
@@ -0,0 +1,46 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+bool canDistribute(vector<int> &candies, long long k, int mid)
+{
+    if (mid == 0)
+    {
+        return false; // Avoid division by zero
+    }
+    long long count = 0;
+    for (auto c : candies)
+    {
+        count += (c / mid); // How many piles of 'mid' can we form?
+        if (count >= k)
+            return true; // If we can satisfy k children, return true
+    }
+    return false;
+}
+int maximumCandies(vector<int> &candies, long long k)
+{
+    int left = 1;
+    int right = *max_element(candies.begin(), candies.end());
+    int ans = 0;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if (canDistribute(candies, k, mid))
+        {
+            ans = mid;      // Store the valid mid
+            left = mid + 1; // Try for a larger value
+        }
+        else
+        {
+            right = mid - 1; // Reduce max value
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+
+    return 0;
+}
