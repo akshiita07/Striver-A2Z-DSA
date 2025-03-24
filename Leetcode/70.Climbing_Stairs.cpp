745cdd883ce0b0730e69dfc71c37c9491c88e526
Akshita Pathak <pathakshita07@gmail.com>
2025-03-24 11:41:19 +0530

    DP

---
+++ Leetcode/70.Climbing_Stairs.cpp
@@ -0,0 +1,52 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int climbStairs(int n)
+{
+    // climb either 1 or 2 steps each time
+
+    // base case: if standing at 0th stair then only 1 way:
+    if (n == 0)
+    {
+        return 1;
+    }
+
+    // base case: if standing at 1th stair then only 1 way:
+    if (n == 1)
+    {
+        return 1;
+    }
+    // jump 1: go 1 index
+    int left = climbStairs(n - 1);
+    // jump 2: go 2nd index
+    int right = climbStairs(n - 2);
+    return left + right;
+}
+
+// TABULATION: WITH SPACE OPTIMIZATION
+int climbStairs(int n)
+{
+    // no of ways to making n using 1s & 2s
+    if (n <= 1)
+    {
+        // base case:
+        return 1; // only 1 possible way
+    }
+    int count1 = 1;
+    int count2 = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        int curr = count1 + count2;
+        count1 = count2;
+        count2 = curr;
+    }
+    return count2;
+}
+
+int main()
+{
+    int n = 3;
+    cout << "\ndistinct ways can you climb to the top= " << climbStairs(n);
+
+    return 0;
+}
---
+++ Leetcode/746.Min_Cost_Climbing_Stairs.cpp
@@ -0,0 +1,17 @@
+﻿class Solution
+{
+public:
+    int minCostClimbingStairs(vector<int> &cost)
+    {
+        // climb 1/2 steps
+        // can start either at 0 or 1
+        int n = cost.size();
+        vector<int> dp(n + 2, 0);
+        // cost at nth top floor=0
+        for (int i = n - 1; i >= 0; i--)
+        {
+            dp[i] = cost[i] + min(dp[i + 1], dp[i + 2]);
+        }
+        return min(dp[0], dp[1]);
+    }
+};
