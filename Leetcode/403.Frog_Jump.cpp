a132044b303530891b7ddb136c3b89971524eb1e
Akshita Pathak <pathakshita07@gmail.com>
2025-04-15 11:52:39 +0530

    potd

---
+++ Leetcode/403.Frog_Jump.cpp
@@ -0,0 +1,15 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+bool canCross(vector<int> &stones)
+{
+    // if prev jump was of k units then next jump must be of either k-1,k,k+1 units
+    // first jump is always 1 unit
+}
+
+int main()
+{
+    vector<int> stones = {0, 1, 3, 5, 6, 8, 12, 17};
+    cout << "\nCan the frog cross the river? " << canCross(stones);
+    return 0;
+}
