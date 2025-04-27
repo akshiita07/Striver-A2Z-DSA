e5129ee3f19145a923076108a6567c6e17dcc1c3
Akshita Pathak <pathakshita07@gmail.com>
2025-04-27 09:17:31 +0530

    potd

---
+++ Leetcode/3392.Count_Subarrays_of_Length_Three_With_a_Condition.cpp
@@ -0,0 +1,25 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int countSubarrays(vector<int> &nums)
+{
+    // return subarrays of length 3 s.t. sum of 1st and 3rd no = half of 2nd no.
+    int n = nums.size();
+    int count = 0;
+    for (int i = 0; i < n - 2; i++)
+    {
+        if ((nums[i] + nums[i + 2]) * 2 == nums[i + 1])
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    vector<int> nums = {1, 2, 1, 4, 1};
+    cout << "\nThe count of fixed-bound subarrays is: " << countSubarrays(nums);
+
+    return 0;
+}
