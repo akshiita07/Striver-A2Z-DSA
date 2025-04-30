855b5103c78da270204ac37c276c54133bef188c
Akshita Pathak <pathakshita07@gmail.com>
2025-04-30 09:01:38 +0530

    potd

---
+++ Leetcode/2465.Number_of_Distinct_Averages.cpp
@@ -0,0 +1,22 @@
+﻿class Solution
+{
+public:
+    int distinctAverages(vector<int> &nums)
+    {
+        int n = nums.size();
+        set<double> distinctAvg;
+        sort(nums.begin(), nums.end());
+        int left = 0;
+        int right = n - 1;
+        while (left <= right)
+        {
+            int mini = nums[left];
+            int maxi = nums[right];
+            double avg = (mini + maxi) / 2.0;
+            distinctAvg.insert(avg);
+            left++;
+            right--;
+        }
+        return distinctAvg.size(); // will give distinct set elements
+    }
+};
