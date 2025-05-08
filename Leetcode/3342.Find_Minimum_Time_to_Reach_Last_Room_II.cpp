86e1cef87cbc10a642c8faf93f0aef2fc9e358a1
Akshita Pathak <pathakshita07@gmail.com>
2025-05-08 07:56:25 +0530

    potd

---
+++ Leetcode/3342.Find_Minimum_Time_to_Reach_Last_Room_II.cpp
@@ -0,0 +1,66 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+// alternating cost movement is added to same 3341 problem
+// we will add parity: 0 for even: 1s for next move, 1 for odd: 2s for next
+// move
+int minTimeToReach(vector<vector<int>> &moveTime)
+{
+    int n = moveTime.size();
+    int m = moveTime[0].size();
+    // moveTime[i][j] = minimum time in seconds when you can start moving to
+    // that room Moving bw adjacent rooms takes one second for one move and
+    // two seconds for the next, alternating between the two. Return the
+    // minimum time to reach the room (n - 1, m - 1)
+    vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+    // directions: up,down,left,right
+    vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    // {time,x,y,parity}
+    priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+    pq.push({0, 0, 0, 0}); // start at {0,0} at time=0 with pairty=0
+    dist[0][0] = 0;
+
+    while (!pq.empty())
+    {
+        auto top = pq.top();
+        pq.pop();
+        int time = top[0];
+        int x = top[1];
+        int y = top[2];
+        int parity = top[3];
+        if (x == n - 1 && y == m - 1) // reached destination
+        {
+            return time;
+        }
+
+        for (auto [dx, dy] : directions)
+        {
+            int nx = x + dx;
+            int ny = y + dy;
+
+            // check if newX,newY is within bounds
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m)
+            {
+                int cost = (parity & 1) ? 2 : 1;
+                int arrival = max(time, moveTime[nx][ny]) + cost;
+                if (arrival < dist[nx][ny])
+                {
+                    dist[nx][ny] = arrival;
+                    pq.push({arrival, nx, ny, parity + 1}); // push with new parity
+                }
+            }
+        }
+    }
+
+    // destination cell (n-1,m-1)
+    return -1;
+}
+
+int main()
+{
+    vector<vector<int>> moveTime = {{0, 4}, {4, 4}};
+    cout << "\nMinimum time to reach last room: " << minTimeToReach(moveTime);
+
+    return 0;
+}
--- Leetcode/tempCodeRunnerFile.cpp
+++ Leetcode/tempCodeRunnerFile.cpp
@@ -1,43 +1,67 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long countTill(long long n, string &s, int limit)
+// alternating cost movement is added to same 3341 problem
+// we will add parity: 0 for even: 1s for next move, 1 for odd: 2s for next move
+int minTimeToReach(vector<vector<int>> &moveTime)
 {
-    // count all powerful integers <= n
-    // generate all possible numbers like s, _s, _ _s and so on
-    // check if it is less than or equal to n and each digit is atMost limit
+    int n = moveTime.size();
+    int m = moveTime[0].size();
+    // moveTime[i][j] = minimum time in seconds when you can start moving to that room
+    // Moving bw adjacent rooms takes one second for one move and two seconds for the next, alternating between the two.
+    // Return the minimum time to reach the room (n - 1, m - 1)
+    vector<vector<vector<int>>> dist(n, vector<vector<int>>(m, vector<int>(2,INT_MAX)));    // shortest time to reach each cell 3d with added parity
+    vector<vector<bool>> visited(n, vector<bool>(m, false)); // visited matrix
+    // directions: up,down,left,right
+    vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-    long long count = 0;
-    int length = s.length();
-    for (int i = 0; i < length; i++)
+    // {time,x,y,parity}
+    priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>, greater<>> pq;
+    pq.push({0, 0,0,0});    //start at {0,0} at time=0 with parity 0
+    dist[0][0][0] = 0;
+
+    while (!pq.empty())
     {
-        // generate number like s, _s, _ _s and so on
-        string temp = s.substr(i);
-        long long num = stoll(temp);    
-        if (num <= n)
+        auto [time, x,y,parity] = pq.top();
+        pq.pop();
+        if(x==n-1 && y=m-1) // reached destination
         {
-            count++;
+            return time;
+        }
+
+        for (auto [dx, dy] : directions)
+        {
+            int nx = x + dx;
+            int ny = y + dy;
+
+            // check if newX,newY is within bounds
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m)
+            {
+                int cost;
+                if(parity==0){
+                    cost=1;
+                }else if(parity==1){
+                    cost=2;
+                }
+                int arrival = max(time+cost, moveTime[nx][ny]);
+                int nextParity=1-parity; // toggle parity
+                if (arrival < dist[nx][ny][nextParity])
+                {
+                    dist[nx][ny][nextParity] = arrival;
+                    pq.push({arrival, nx, ny,nextParity}); // push with new parity
+                }
+            }
         }
     }
-    return count;
-}
 
-long long numberOfPowerfulInt(long long start, long long finish, int limit, string s)
-{
-    // integer x is powerful if it ends with s & each digit of x is atMost limit(limit or less)
-    // return total no of powerful integrs in range [start, finish]
-
-    // generate numbers like s,_s,_ _s and so on
-    return (countTill(finish, s, limit) - countTill(start - 1, s, limit));
+    // destination cell (n-1,m-1)
+    return min(dist[n - 1][m - 1][0],dist[n - 1][m - 1][1]);
 }
 
 int main()
 {
-    long long start = 1;
-    long long finish = 6000;
-    int limit = 4;
-    string s = "124";
-    cout << "\nNumber of powerful integers: " << numberOfPowerfulInt(start, finish, limit, s);
+    vector<vector<int>> moveTime = {{0, 4}, {4, 4}};
+    cout << "\nMinimum time to reach last room: " << minTimeToReach(moveTime);
 
     return 0;
 }
