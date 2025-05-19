bff2f960da42e0db34883a1b6f37367722a5ea84
Akshita Pathak <pathakshita07@gmail.com>
2025-05-19 10:39:07 +0530

    potd

---
+++ Leetcode/3024.Type_of_Triangle.cpp
@@ -0,0 +1,29 @@
+﻿class Solution
+{
+public:
+    string triangleType(vector<int> &nums)
+    {
+        int side1 = nums[0];
+        int side2 = nums[1];
+        int side3 = nums[2];
+        // Check if the sides can form a valid triangle first
+        if (side1 + side2 <= side3 || side1 + side3 <= side2 ||
+            side2 + side3 <= side1)
+        {
+            return "none"; // If it cannot form a triangle
+        }
+
+        if (side1 == side2 && side2 == side3)
+        {
+            return "equilateral";
+        }
+        if ((side1 == side2 && side2 != side3) ||
+            (side1 == side3 && side1 != side2) ||
+            (side2 == side3 && side1 != side2))
+        {
+            return "isosceles";
+        }
+        // some of any 2 sides>third side for scalene
+        return "scalene";
+    }
+};
