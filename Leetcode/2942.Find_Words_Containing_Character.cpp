fb562544fc9d7d635faab4c6a42244ba482e7556
Akshita Pathak <pathakshita07@gmail.com>
2025-05-24 06:22:28 +0530

    potd

---
+++ Leetcode/2942.Find_Words_Containing_Character.cpp
@@ -0,0 +1,21 @@
+﻿class Solution
+{
+public:
+    vector<int> findWordsContaining(vector<string> &words, char x)
+    {
+        vector<int> ans;
+        for (int i = 0; i < words.size(); i++)
+        {
+            // now for each word traverse till end to find character x:
+            for (int j = 0; j < words[i].length(); j++)
+            {
+                if (words[i][j] == x)
+                {
+                    ans.push_back(i);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
