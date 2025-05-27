753c7ca8dc041df863e2648f30a6b16f8dd3fd6c
Akshita Pathak <pathakshita07@gmail.com>
2025-05-27 07:12:23 +0530

    potd

---
+++ Leetcode/2894.Divisible_and_Non-divisible_Sums_Difference.cpp
@@ -0,0 +1,26 @@
+﻿class Solution
+{
+public:
+    int differenceOfSums(int n, int m)
+    {
+        int num1 = 0;
+        int num2 = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            // not div by m
+            if (i % m != 0)
+            {
+                num1 += i;
+            }
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            // div by m
+            if (i % m == 0)
+            {
+                num2 += i;
+            }
+        }
+        return num1 - num2;
+    }
+};
