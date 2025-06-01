58215d68c5f341bdf9f52f7d7b801defae34a813
Akshita Pathak <pathakshita07@gmail.com>
2025-06-01 07:55:38 +0530

    potd

---
+++ Leetcode/2928.Distribute_Candies_Among_Children_I.cpp
@@ -0,0 +1,31 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int distributeCandies(int n, int limit)
+{
+    //   distribute n candies among 3 children such that no child gets more than limit candies
+    int count = 0;
+    for (int i = 0; i <= limit; i++)
+    {
+        for (int j = 0; j <= limit; j++)
+        {
+            for (int k = 0; k <= limit; k++)
+            {
+                if (i + j + k == n)
+                {
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    int n = 5;
+    int limit = 2;
+    cout << "\nNumber of ways are= " << distributeCandies(n, limit);
+
+    return 0;
+}
---
+++ Leetcode/2929.Distribute_Candies_Among_Children_II.cpp
@@ -0,0 +1,33 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+long long distributeCandies(int n, int limit)
+{
+    //   distribute n candies among 3 children such that no child gets more than limit candies
+    // Hint 1
+    // We can enumerate the number of candies of one particular child, let it be i which means 0 <= i <= min(limit, n).
+    long long count = 0;
+    for (int i = 0; i <= min(n, limit); i++)
+    {
+        // Hint 2
+        // Suppose the 2nd child gets j candies. Then 0 <= j <= limit and i + j <= n.
+        int minJ = max(0, n - limit - i);
+        int maxJ = min(limit, n - i);
+        // Hint 3
+        // The 3rd child will hence get n - i - j candies and we should have 0 <= n - i - j <= limit.
+        if (minJ <= maxJ)
+        {
+            count += (maxJ - minJ + 1);
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    int n = 5;
+    int limit = 2;
+    cout << "\nNumber of ways are= " << distributeCandies(n, limit);
+
+    return 0;
+}
