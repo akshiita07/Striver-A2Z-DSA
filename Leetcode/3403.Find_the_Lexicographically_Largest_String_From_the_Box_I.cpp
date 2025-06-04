defce51f0d810e9cad2acd852498fb37fa03f1fb
Akshita Pathak <pathakshita07@gmail.com>
2025-06-04 09:36:49 +0530

    potd

---
+++ Leetcode/3403.Find_the_Lexicographically_Largest_String_From_the_Box_I.cpp
@@ -0,0 +1,32 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+string answerString(string word, int numFriends)
+{
+    // number of friends: numFriends
+    // split "word" into numFriends parts st prev round did not have same split
+    // Find the lexicographically largest string
+    int n = word.size();
+    int minLen = n - numFriends + 1;
+    string ans = "";
+    for (int i = 0; i <= n - minLen; i++)
+    {
+        // try all substrings of this length
+        string curr = word.substr(i, minLen);
+        if (curr > ans)
+        {
+            // take lexicographically largest string:
+            ans = curr;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    string word = "dbca";
+    int numFriends = 2;
+    cout << "\nlexicographically largest string= " << answerString(word, numFriends);
+
+    return 0;
+}
