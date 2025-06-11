3a08d191e0da3f488a7e2c44865feaae28fd8767
Akshita Pathak <pathakshita07@gmail.com>
2025-06-11 23:49:02 +0530

    potd

--- Leetcode/3445.Maximum_Difference_Between_Even_and_Odd_Frequency_II.cpp
+++ Leetcode/3445.Maximum_Difference_Between_Even_and_Odd_Frequency_II.cpp
@@ -6,7 +6,12 @@ int maxDifference(string s, int k)
     // find maximum difference bw frequency of two characters, freq[a] - freq[b], in a substring subs of s st subs has a size of at least k & a:odd freq, b:even freq
     // Return the maximum difference.
     // string s contains only of digits '0' to '4'
-    int n = s.length();
+    auto getStatus = [](int cnt_a, int cnt_b) -> int
+    {
+        return ((cnt_a & 1) << 1) | (cnt_b & 1);
+    };
+
+    int n = s.size();
     int ans = INT_MIN;
     for (char a = '0'; a <= '4'; a++)
     {
@@ -16,32 +21,31 @@ int maxDifference(string s, int k)
             {
                 continue;
             }
-            vector<int> freq(5, 0);
-            int left = 0;
+            int best[4] = {INT_MAX, INT_MAX, INT_MAX, INT_MAX};
+            int cnt_a = 0;
+            int cnt_b = 0;
+            int prev_a = 0;
+            int prev_b = 0;
+            int left = -1;
             for (int right = 0; right < n; right++)
             {
-                // freq increment:
-                freq[s[right] - '0']++;
-                while (right - left + 1 > k)
+                cnt_a += (s[right] == a);
+                cnt_b += (s[right] == b);
+                while (right - left >= k && cnt_b - prev_b >= 2)
                 {
-                    freq[s[left] - '0']--;
+                    int left_status = getStatus(prev_a, prev_b);
+                    best[left_status] = min(best[left_status], prev_a - prev_b);
                     left++;
+                    prev_a += (s[left] == a);
+                    prev_b += (s[left] == b);
                 }
-                if (right - left + 1 >= k)
+                int right_status = getStatus(cnt_a, cnt_b);
+                if (best[right_status ^ 0b10] != INT_MAX)
                 {
-                    int aFreq = freq[a - '0'];
-                    int bFreq = freq[b - '0'];
-                    if (aFreq % 2 != 0 && bFreq % 2 == 0)
-                    {
-                        ans = max(ans, aFreq - bFreq);
-                    }
+                    ans = max(ans, cnt_a - cnt_b - best[right_status ^ 0b10]);
                 }
             }
         }
-    }
-    if (ans == INT_MIN)
-    {
-        return -1;
     }
     return ans;
 }
