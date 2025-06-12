2cda63c25e69b750e3f95148c4bf479e9eafdaa4
Akshita Pathak <pathakshita07@gmail.com>
2025-06-12 22:55:04 +0530

    potd

---
+++ Leetcode/3423.Maximum_Difference_Between_Adjacent_Elements_in_a_Circular_Array.cpp
@@ -0,0 +1,24 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int maxAdjacentDistance(vector<int> &nums)
+{
+    // find max absolute diff between adjacent elements
+    // circular array ie 1st and last elements are circular
+    int n = nums.size();
+    int ans = INT_MIN;
+    for (int i = 0; i < n - 1; i++)
+    {
+        ans = max(ans, abs((nums[i]) - (nums[i + 1])));
+    }
+    ans = max(ans, abs((nums[0]) - (nums[n - 1])));
+    return ans;
+}
+
+int main()
+{
+    vector<int> nums = {1, 2, 4};
+    cout << "\nMaximum Adjacent Distance: " << maxAdjacentDistance(nums);
+
+    return 0;
+}
