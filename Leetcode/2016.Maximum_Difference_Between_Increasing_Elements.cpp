4781ca211b741c751dff2db6f85247c955fee295
Akshita Pathak <pathakshita07@gmail.com>
2025-06-16 09:34:01 +0530

    potd

---
+++ Leetcode/2016.Maximum_Difference_Between_Increasing_Elements.cpp
@@ -0,0 +1,28 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int maximumDifference(vector<int> &nums)
+{
+    int n = nums.size();
+    // find max diff nums[j]-nums[i] st 0 <= i < j < n and nums[i] < nums[j] else return -1
+    int maxDiff = -1;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (nums[i] < nums[j])
+            {
+                maxDiff = max(maxDiff, nums[j] - nums[i]);
+            }
+        }
+    }
+    return maxDiff;
+}
+
+int main()
+{
+    vector<int> nums = {7, 1, 5, 4};
+    cout << "\nMaximum Difference: " << maximumDifference(nums);
+
+    return 0;
+}
