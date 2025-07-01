55a14a044a662179a52a9881b92ccf9acd00271b
Akshita Pathak <pathakshita07@gmail.com>
2025-07-01 09:58:59 +0530

    potd

---
+++ Leetcode/3330.Find_the_Original_Typed_String_I.cpp
@@ -0,0 +1,36 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int possibleStringCount(string word)
+{
+    //  atmost 1 or 0 errors
+    // Return total number of possible original strings that Alice might have intended to type
+    int n = word.length();
+    int ans = 1;
+    for (int i = 0; i < n; i++)
+    {
+        // Count the number of consecutive characters
+        int count = 1;
+        while (i + 1 < n && word[i] == word[i + 1])
+        {
+            count++;
+            i++;
+        }
+        if (count > 1)
+        {
+            while (count > 1)
+            {
+                ans++;
+                count--;
+            }
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    string word = "abbcccca";
+    cout << "\ntotal number of possible original strings= " << possibleStringCount(word);
+    return 0;
+}
