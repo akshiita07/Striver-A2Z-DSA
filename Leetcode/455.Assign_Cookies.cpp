551061cbad9dd59e0e7b1ed057d67ab86ab22ef4
Akshita Pathak <pathakshita07@gmail.com>
2025-07-13 11:12:22 +0530

    ques

---
+++ Leetcode/455.Assign_Cookies.cpp
@@ -0,0 +1,41 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int findContentChildren(vector<int> &g, vector<int> &s)
+{
+    // give each child at most one cookie
+    // g[i]= greed factor of each child
+    // s[j]= size of cookie j
+    // assign cookie j to child i if s[j] >= g[i]
+    // maximize the number of your content children and output the maximum number
+    sort(g.begin(), g.end());
+    sort(s.begin(), s.end());
+    int n = g.size();
+    int m = s.size();
+    int i = 0;
+    int j = 0;
+    int contentChildren = 0;
+    while (i < n && j < m)
+    {
+        if (s[j] >= g[i])
+        {
+            contentChildren++;
+            i++;
+            j++;
+        }
+        else if (s[j] < g[i])
+        {
+            j++;
+        }
+    }
+    return contentChildren;
+}
+
+int main()
+{
+    vector<int> g = {1, 2};
+    vector<int> s = {1, 2, 3};
+    cout << "\nnumber of your content children= " << findContentChildren(g, s);
+
+    return 0;
+}
