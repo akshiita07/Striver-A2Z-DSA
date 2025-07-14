0e01c01acc9bcedbb9535f4fa8532a35997d6f6a
Akshita Pathak <pathakshita07@gmail.com>
2025-07-14 09:50:27 +0530

    potd

---
+++ Leetcode/1290.Convert_Binary_Number_in_a_Linked_List_to_Integer.cpp
@@ -0,0 +1,50 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+int getDecimalValue(ListNode *head)
+{
+    // first form the decimal number as string
+    string num = "";
+    ListNode *temp = head;
+    while (temp != nullptr)
+    {
+        // cout<<temp->val<<" ";
+        num += to_string(temp->val);
+        temp = temp->next;
+    }
+    // convert num to integer
+    cout << "\nNumber is: " << num;
+
+    // int binary=stoi(num);
+    // cout<<"\nNumber in integer is: "<<binary;
+
+    // convert from binary to decimal
+    // multiply by 2^power * digit
+    int dec = 0;
+    int base = 1;
+    for (int i = num.length() - 1; i >= 0; i--)
+    {
+        if (num[i] == '1')
+        {
+            dec += base;
+        }
+        base = base * 2;
+    }
+    return dec;
+}
+
+int main()
+{
+
+    return 0;
+}
