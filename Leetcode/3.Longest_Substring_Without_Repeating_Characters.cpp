da97cbb999d5031db113ee1b0c8b394e469dbc89
Akshita Pathak <pathakshita07@gmail.com>
2025-07-22 23:09:13 +0530

    practise

---
+++ Leetcode/3.Longest_Substring_Without_Repeating_Characters.cpp
@@ -0,0 +1,38 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int lengthOfLongestSubstring(string s)
+{
+    // find the length of the longest substring without duplicate characters
+    unordered_set<char> duplicate;
+    int n = s.length();
+    int left = 0;
+    int right = 0;
+    int maxLength = INT_MIN;
+    int currLen = 0;
+    while (right < n)
+    {
+        // check if already present:
+        while (duplicate.find(s[right]) != duplicate.end())
+        {
+            // remove & shorten window
+            duplicate.erase(s[left]);
+            currLen--;
+            left++;
+        }
+        // add rightmost element:
+        duplicate.insert(s[right]);
+        currLen++;
+        maxLength = max(maxLength, currLen);
+        right++;
+    }
+    return maxLength;
+}
+
+int main()
+{
+    string s = "pwwkew";
+    cout << "\nLength of the longest substring without repeating characters= " << lengthOfLongestSubstring(s);
+
+    return 0;
+}
