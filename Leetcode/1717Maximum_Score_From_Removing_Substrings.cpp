8bbe937a7f198cb9a902bc0f3a09777a6bd983d6
Akshita Pathak <pathakshita07@gmail.com>
2025-07-23 10:04:46 +0530

    potd

---
+++ Leetcode/1717Maximum_Score_From_Removing_Substrings.cpp
@@ -0,0 +1,62 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int remove(string &s, char first, char second, int points)
+{
+    stack<char> st;
+    int score = 0;
+    for (char ch : s)
+    {
+        if (!st.empty() && st.top() == first && ch == second)
+        {
+            // foud substring 'first second'
+            st.pop();
+            score += points;
+        }
+        else
+        {
+            st.push(ch);
+        }
+    }
+    // after removing all substrings rebuild string:
+    s = "";
+    while (!st.empty())
+    {
+        s += st.top();
+        st.pop();
+    }
+    reverse(s.begin(), s.end());
+    return score;
+}
+
+int maximumGain(string s, int x, int y)
+{
+    //  Remove substring "ab" and gain x points
+    // Remove substring "ba" and gain y points
+    // Return the maximum points you can gain
+    int score = 0;
+    // will try to always remove substring that gives HIGHER points first
+    if (x > y)
+    {
+        // remove 'ab' first
+        score += remove(s, 'a', 'b', x);
+        score += remove(s, 'b', 'a', y);
+    }
+    else
+    {
+        // remove 'ba' first
+        score += remove(s, 'b', 'a', y);
+        score += remove(s, 'a', 'b', x);
+    }
+    return score;
+}
+
+int main()
+{
+    string s = "cdbcbbaaabab";
+    int x = 4;
+    int y = 5;
+    cout << "\nMaximum score from removing substrings= " << maximumGain(s, x, y);
+
+    return 0;
+}
--- Leetcode/tempCodeRunnerFile.cpp
+++ Leetcode/tempCodeRunnerFile.cpp
@@ -1,4 +1,34 @@
-if (1LL * zerosInOther * 9 >= diff)
+// event occurs from time t to eventTime
+    int n = startTime.size(); //== endTime.size();
+    // ith meeting occurs during the time [startTime[i], endTime[i]]
+    // reschedule at most k meetings by moving their start time while maintaining the same duration, to maximize the longest continuous period of free time
+    // Return the maximum amount of free time possible after rearranging the meetings
+    vector<int> gaps;
+    // gap before the first meeting:
+    gaps.push_back(startTime[0] - 0);
+    // gap between meetings:
+    for (int i = 1; i < n; i++)
     {
-        possible = true;
+        // start of next & end of prev:
+        gaps.push_back(startTime[i] - endTime[i - 1]);
     }
+    // gap after the last meeting:
+    gaps.push_back(eventTime - endTime[n - 1]);
+    int m = gaps.size();
+    // sliding window:
+    int windowSize = k + 1;
+    int maxFreeTime = 0;
+    int sum = 0;
+    for (int i = 0; i < m; i++)
+    {
+        sum += gaps[i];
+        if (i >= windowSize)
+        {
+            sum -= gaps[i - windowSize];
+        }
+        if (i >= windowSize - 1)
+        {
+            maxFreeTime = max(maxFreeTime, sum);
+        }
+    }
+    return maxFreeTime;
