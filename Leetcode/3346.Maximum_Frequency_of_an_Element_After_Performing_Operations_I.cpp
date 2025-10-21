dc111c46ccb7e206ab2de419c89491e9bb400392
Akshita Pathak <pathakshita07@gmail.com>
2025-10-21 10:50:35 +0530

    potd

---
+++ Leetcode/3346.Maximum_Frequency_of_an_Element_After_Performing_Operations_I.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxFrequency(vector<int> &nums, int k, int numOperations)
+{
+    // perform an operation numOperations times on nums
+    // select integer i and add number between range [-k, k] to nums[i]
+    // Return the maximum possible frequency of any element
+    sort(nums.begin(), nums.end());
+    int ans = 0;
+    unordered_map<int, int> numCount;
+
+    int lastNumIndex = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] != nums[lastNumIndex])
+        {
+            numCount[nums[lastNumIndex]] = i - lastNumIndex;
+            ans = max(ans, i - lastNumIndex);
+            lastNumIndex = i;
+        }
+    }
+
+    numCount[nums[lastNumIndex]] = nums.size() - lastNumIndex;
+    ans = max(ans, (int)nums.size() - lastNumIndex);
+
+    auto leftBound = [&](int value)
+    {
+        int left = 0, right = nums.size() - 1;
+        while (left < right)
+        {
+            int mid = (left + right) / 2;
+            if (nums[mid] < value)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid;
+            }
+        }
+        return left;
+    };
+
+    auto rightBound = [&](int value)
+    {
+        int left = 0, right = nums.size() - 1;
+        while (left < right)
+        {
+            int mid = (left + right + 1) / 2;
+            if (nums[mid] > value)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid;
+            }
+        }
+        return left;
+    };
+
+    for (int i = nums.front(); i <= nums.back(); i++)
+    {
+        int l = leftBound(i - k);
+        int r = rightBound(i + k);
+
+        int tempAns;
+        if (numCount.count(i))
+        {
+            tempAns = min(r - l + 1, numCount[i] + numOperations);
+        }
+        else
+        {
+            tempAns = min(r - l + 1, numOperations);
+        }
+        ans = max(ans, tempAns);
+    }
+
+    return ans;
+}
+
+int main()
+{
+
+    return 0;
+}
