48510f2bc8a4a3871e2c78ccd49a855f21950fff
Akshita Pathak <pathakshita07@gmail.com>
2025-11-30 11:01:18 +0530

    potd

--- Leetcode/1590.MakeSumDivisibleByP.cpp
+++ Leetcode/1590.MakeSumDivisibleByP.cpp
@@ -38,6 +38,46 @@ int minSubarray(vector<int> &nums, int p)
     return minLength == nums.size() ? -1 : minLength;
 }
 
+// Leetcode:
+class Solution {
+public:
+    int minSubarray(vector<int>& nums, int p) {
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum = (totalSum + num) % p;
+        }
+
+        // If the total sum is already divisible by p, no need to remove any
+        // subarray
+        if (totalSum == 0) {
+            return 0;
+        }
+
+        // To find the smallest subarray to remove, we use prefix sums and hashing
+        unordered_map<int, int> prefixSumRemainder;
+        prefixSumRemainder[0] = -1; // To handle cases where the subarray starts from index 0
+        int prefixSum = 0;
+        int minLength = nums.size();
+        for (int i = 0; i < nums.size(); i++) {
+            prefixSum = (prefixSum + nums[i]) % p;
+            int target = (prefixSum - totalSum + p) % p;
+
+            // If target remainder is found in the map, calculate the subarray
+            // length
+            if (prefixSumRemainder.find(target) != prefixSumRemainder.end()) {
+                minLength = min(minLength, i - prefixSumRemainder[target]);
+            }
+
+            // Update the map with the current prefix sum remainder
+            prefixSumRemainder[prefixSum] = i;
+        }
+
+        // If the minimum length found is equal to the size of the array, return
+        // -1
+        return minLength == nums.size() ? -1 : minLength;
+    }
+};
+
 int main()
 {
     vector<int> nums = {3, 1, 4, 2};
