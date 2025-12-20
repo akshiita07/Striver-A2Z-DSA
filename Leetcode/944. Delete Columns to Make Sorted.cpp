4d832d44b469c64d6e4600c5f142893f8181859f
Akshita Pathak <pathakshita07@gmail.com>
2025-12-20 10:43:31 +0530

    potd

---
+++ Leetcode/944. Delete Columns to Make Sorted.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int n = strs.size();
+        int m = strs[0].size();
+        int count = 0;
+
+        for (int col = 0; col < m; col++) {
+            for (int row = 1; row < n; row++) {
+                if (strs[row][col] < strs[row - 1][col]) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+};
