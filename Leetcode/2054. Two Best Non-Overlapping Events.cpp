5cf126e3b4eb92f1691d28156b931dbe58bc37f0
Akshita Pathak <pathakshita07@gmail.com>
2025-12-23 21:59:26 +0530

    potd

---
+++ Leetcode/2054. Two Best Non-Overlapping Events.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    // comparator lambda fnc to sort events based on thier end times:
+    // bool compareEndTimes(const vector<int>& a, const vector<int>& b) {
+    //     if (a[1] < b[1]) {
+    //         return true;
+    //     }
+    //     return false;
+    // }
+
+    int maxTwoEvents(vector<vector<int>>& events) {
+        // events[i] = [startTimei, endTimei, valuei]
+        // if you attend an event with end time t, the next event must start at
+        // or after t + 1 choose 2 or less non-overlapping events to attend st
+        // sum of their values is maximized. return max sum
+        int n = events.size();
+        // sort events based on thier end times using a comparator lambda fnc:
+        sort(events.begin(), events.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[1] < b[1];
+        });
+        // all max values to be stored
+        vector<int> maxValues(n, 0);
+        // add values at [0,1,2] ie 2nd index is value
+        maxValues[0] = events[0][2];
+        for (int i = 1; i < n; i++) {
+            // fill max values for all elements:
+            maxValues[i] = max(events[i][2], maxValues[i - 1]);
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            // take this event
+            ans = max(ans, events[i][2]);
+            // choose next possible non overlapping event that maximises ans
+            //  binary search:
+            int best = -1;
+                // end time of prev event<start time of next event
+            int left=0;
+            int right=i-1;
+            while(left<=right){
+                int mid=(left+right)/2;
+                if(events[mid][1]<events[i][0]){
+                    best=mid;
+                    left=mid+1;
+                }else{
+                    right=mid-1;
+                }
+            }
+
+            if (best != -1) {
+                ans = max(ans, events[i][2] + maxValues[best]);
+            }
+        }
+        return ans;
+    }
+};
