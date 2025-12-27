5c8b35560d3493057904e8fa77e7b16b86f2eb11
Akshita Pathak <pathakshita07@gmail.com>
2025-12-27 10:56:57 +0530

    potd

---
+++ Leetcode/2402. Meeting Rooms III.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int mostBooked(int n, vector<vector<int>>& meetings) {
+        // meetings[i] = [starti, endi)
+        // meeting in unused room with lowest number
+        // Return the number of the room that held the most meetings
+        vector<long long> roomAvailabilityTime(n, 0);
+        vector<int> meetingCount(n, 0);
+        sort(meetings.begin(), meetings.end());
+
+        for (auto& meeting : meetings) {
+            int start = meeting[0];
+            int end = meeting[1];
+            long long minRoomAvailabilityTime = LLONG_MAX;
+            int minAvailableTimeRoom = 0;
+            bool foundUnusedRoom = false;
+
+            for (int i = 0; i < n; i++) {
+                if (roomAvailabilityTime[i] <= start) {
+                    foundUnusedRoom = true;
+                    meetingCount[i]++;
+                    roomAvailabilityTime[i] = end;
+                    break;
+                }
+
+                if (minRoomAvailabilityTime > roomAvailabilityTime[i]) {
+                    minRoomAvailabilityTime = roomAvailabilityTime[i];
+                    minAvailableTimeRoom = i;
+                }
+            }
+
+            if (!foundUnusedRoom) {
+                roomAvailabilityTime[minAvailableTimeRoom] += end - start;
+                meetingCount[minAvailableTimeRoom]++;
+            }
+        }
+        int maxMeetingCount = 0, maxMeetingCountRoom = 0;
+        for (int i = 0; i < n; i++) {
+            if (meetingCount[i] > maxMeetingCount) {
+                maxMeetingCount = meetingCount[i];
+                maxMeetingCountRoom = i;
+            }
+        }
+        return maxMeetingCountRoom;
+    }
+};
