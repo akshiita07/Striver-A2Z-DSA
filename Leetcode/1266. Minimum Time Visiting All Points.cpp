61e93ec565286e2785e05c94e8bc9d15a46d603b
Akshita Pathak <pathakshita07@gmail.com>
2026-01-12 10:31:39 +0530

    potd

---
+++ Leetcode/1266. Minimum Time Visiting All Points.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int n=points.size();
+        int ans = 0;
+
+        for (int i = 1; i <n; i++) {
+            int dx = abs(points[i][0] - points[i-1][0]);
+            int dy = abs(points[i][1] - points[i-1][1]);
+            ans += max(dx, dy);
+        }
+
+        return ans;
+    }
+};
