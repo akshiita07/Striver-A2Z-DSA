a5cb821518c7474265eaa1b798448e0e652d86b5
Akshita Pathak <pathakshita07@gmail.com>
2026-03-03 10:12:10 +0530

    potd

---
+++ LeetcodePart2/1545. Find Kth Bit in Nth Binary String.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    char findKthBit(int n, int k) {
+        // binary string Sn
+        //     S1 = "0"
+        // Si = S(i-1) + "1" + reverse(invert(S(i-1)) for i > 1
+
+        // Base case: If n = 1, the only bit is '0'
+        if (n == 1) {
+            return '0';
+        }
+
+        // Find the length of Sn
+        int length = (1 << n) - 1; // 2^n - 1
+
+        int mid = (length / 2) + 1; // Middle position
+
+        if (k == mid) {
+            // If k is the middle bit, it's always '1'
+            return '1';
+        } else if (k < mid) {
+            // If k is in the first half, it's in Sn-1
+            return findKthBit(n - 1, k);
+        } else {
+            // If k is in the second half, it's the mirrored and inverted bit
+            // from Sn-1
+            int mirroredK = length - k + 1;
+            char mirroredBit = findKthBit(n - 1, mirroredK);
+            return mirroredBit == '0' ? '1' : '0'; // Invert the bit
+        }
+    }
+};
