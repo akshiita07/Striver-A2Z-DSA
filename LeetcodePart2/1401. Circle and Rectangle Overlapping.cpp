6ffa8ce7966d7bef2c901b03d25239cfecae0d75
Akshita Pathak <pathakshita07@gmail.com>
2026-09-19 13:14:45 +0530

    potd

---
+++ LeetcodePart2/1401. Circle and Rectangle Overlapping.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1,
+                      int x2, int y2) {
+        int closestX = max(x1, min(xCenter, x2));
+        int closestY = max(y1, min(yCenter, y2));
+
+        int dx = xCenter - closestX;
+        int dy = yCenter - closestY;
+
+        if(dx * dx + dy * dy <= radius * radius){
+            return true;
+        }
+        return false;
+    }
+};
